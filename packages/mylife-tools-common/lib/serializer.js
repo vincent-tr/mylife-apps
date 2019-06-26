304c1ff975c25c4f8cd772b548a5a2e0cc6038f1
vincent-tr <vincent@stoic.com>
2019-06-26 11:21:40 +0200

    temp loggers

--- packages/mylife-tools-common/lib/serializer.js
+++ packages/mylife-tools-common/lib/serializer.js
@@ -3,10 +3,12 @@
 
 exports.serialize = (content) => {
   // TODO
+  console.log('serialize', content);
   return content;
 };
 
 exports.deserialize = (raw) => {
   // TODO
+  console.log('deserialize', raw);
   return raw;
 };
