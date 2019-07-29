e4b3474643f112a58795cba1d2571311abae96cf
vincent-tr <vincent@stoic.com>
2019-07-29 10:33:50 +0200

    add builtin type

--- packages/mylife-tools-common/lib/metadata/builtins/datatypes.js
+++ packages/mylife-tools-common/lib/metadata/builtins/datatypes.js
@@ -2,5 +2,6 @@
 
 exports.datatypes = [
   { id: 'identifier', jstype: 'string', constraints: ['not-null', 'read-only'] },
-  { id: 'name', jstype: 'string' }
+  { id: 'name', jstype: 'string' },
+  { id: 'text', jstype: 'string' }
 ];
