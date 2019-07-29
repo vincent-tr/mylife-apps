93c9c300239452b4cbdedc2156d60404b5c63f31
vincent-tr <vincent@stoic.com>
2019-07-29 10:06:44 +0200

    datatype validation

--- packages/mylife-tools-common/lib/metadata/engine/datatype.js
+++ packages/mylife-tools-common/lib/metadata/engine/datatype.js
@@ -2,30 +2,42 @@
 
 const { Constraint } = require('./constraint');
 const registry = require('./registry');
+const { lock, Validator } = reqwuire('./utils');
 
 exports.Datatype = class Datatype {
   constructor(definition) {
+    const validator = new Validator(this);
+
+    validator.validateId(definition.id);
     this._id = definition.id;
+
     this._primitive = definition.primitive:
 
     if(definition.enum) {
+      validator.validate(definition.enum, 'enum', { type: 'string-array', mandatory: true });
       this._primitive = 'enum'
       this._values = definition.enum;
     }
 
     if(definition.reference) {
+      validator.validate(definition.reference, 'reference', { type: 'string', mandatory: true });
       this._primitive = 'reference';
       this._target = definition.reference;
     }
 
     if(definition.collection) {
+      validator.validate(definition.collection, 'reference', { type: 'string', mandatory: true });
       this._primitive = 'collection';
       this._target = definition.collection;
     }
 
-    this._constraints = definition.constraints || [];
+    validator.validate(this._primitive, 'primitive', { type: 'string', mandatory: true });
 
-    // TODO: validation/freeze
+    validator.validate(definition.constraints, 'constraints', { type: 'array' });
+    this._constraints = (definition.constraints || []).map(cdef => new Constraint(cdef));
+
+    Object.freeze(this._constraints);
+    lock(this);
   }
 
   get id() {
---
+++ packages/mylife-tools-common/lib/metadata/engine/utils.js
@@ -0,0 +1,62 @@
+'use strict';
+
+exports.lock = (object) => {
+
+  const config = { enumerable: false };
+  for (const propName of Object.getOwnPropertyNames(obj)) {
+    if(propName.startsWith('_')) {
+      Object.defineProperty(obj, propName, { enumerable: false });
+    }
+  }
+
+  Object.freeze(this);
+};
+
+exports.Validator = class Validator {
+  constructor(obj) {
+    this._objectType = obj.constructor.name;
+  }
+
+  validateId(id) {
+    if(!id) {
+      throw new Error(`Metadata validation failed: id is mandatory for ${objectType}`);
+    }
+    if(typeof id !== 'string') {
+      throw new Error(`Metadata validation failed: id must be 'string' for ${objectType}`);
+    }
+    this._id = id;
+    this._objectString = `${this._objectType}(${this._id})`;
+  }
+
+  validate(value, argumentName, { type, mandatory = false }) {
+    if(value === null || value === undefined) {
+      if(mandatory) {
+        throw new Error(`Metadata validation failed: ${argumentName} is mandatory for ${this._objectString}`);
+      }
+    }
+
+    const matchArray = /^(.*)-array$/.exec(type);
+    if(matchArray) {
+      const itemType = matchArray[1];
+      this.validate(value, argumentName, { type: 'array', mandatory });
+      for(const [i, item] of (value || []).entries()) {
+        this.validate(value, `${argumentName}[${i}]`, { type: itemType, mandatory: true });
+      }
+      return;
+    }
+
+    if(getType(value) !== type) {
+      throw new Error(`Metadata validation failed: ${argumentName} must be '${type}' for ${this._objectString}`);
+    }
+  }
+};
+
+function getType(obj) {
+  if(obj instanceof Function) {
+    return 'function';
+  }
+  if(Array.isArray(obj)) {
+    return 'array';
+  }
+  return typeof(obj);
+}
