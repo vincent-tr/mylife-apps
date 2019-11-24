acc815762880a49653c9a2e7da3f097b82cee290
vincent-tr <vincent.tr@gmail.com>
2019-11-24 16:43:06 +0100

    fix

--- packages/mylife-tools-common/lib/utils/immutable.js
+++ packages/mylife-tools-common/lib/utils/immutable.js
@@ -3,7 +3,7 @@
 // https://redux.js.org/recipes/structuring-reducers/immutable-update-patterns
 
 exports.immutable = {
-  arrayPush: (array, index, value) => [...array, value],
+  arrayPush: (array, value) => [...array, value],
   arrayRemove: (array, index) => [...array.slice(0, index), ...array.slice(index + 1)],
   arrayUpdate: (array, index, value) => array.map((item, mapIndex) => mapIndex === index ? value : item),
 };
