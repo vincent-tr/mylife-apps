05fc0dd7169a63d8fe3c755482d96158eb234c13
vincent-tr <vincent.tr@gmail.com>
2019-12-14 21:00:24 +0100

    cleanup

--- packages/mylife-gallery/public/src/common/document-type.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export const DOCUMENT_TYPES = [
-  { id: 'image', text: 'Image' },
-  { id: 'video', text: 'Vidéo' },
-  { id: 'other', text: 'Autre' }
-];
-
-export const DOCUMENT_TYPE_MAP = new Map(DOCUMENT_TYPES.map(type => [type.id, type.text]));
