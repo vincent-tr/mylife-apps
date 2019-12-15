392a73e2ce331f74d775c203ee16cdf0648e9095
vincent-tr <vincent.tr@gmail.com>
2019-12-15 15:47:29 +0100

    cleanup

--- packages/mylife-gallery/public/src/common/thumbnail/image.js
+++ packages/mylife-gallery/public/src/common/thumbnail/image.js
@@ -4,37 +4,40 @@ import { React, PropTypes, clsx, useState, useEffect } from 'mylife-tools-ui';
 import { getThumbnailUrl, useCommonStyles } from './utils';
 import icons from '../icons';
 
-const ThumbnailImage = ({ document, className, ...props }) => {
+const NotNull = ({ thumbnail }) => {
   const classes = useCommonStyles();
   const [loading, setLoading] = useState(false);
 
   // onLoadStart not fired on chrome
-  useEffect(() => setLoading(true), [document.thumbnail]);
+  useEffect(() => setLoading(true), [thumbnail]);
 
-  let content;
+  const thumbnailUrl = getThumbnailUrl(thumbnail);
+  return (
+    <React.Fragment>
+      <img
+        className={clsx({ [classes.imageLoading]: loading })}
+        src={thumbnailUrl}
+        onLoad={() => setLoading(false)} />
+      {loading && (
+        <icons.documents.Pending className={classes.imageFallback} />
+      )}
+    </React.Fragment>
+  );
+};
 
-  if(document.thumbnail) {
-    const thumbnailUrl = getThumbnailUrl(document.thumbnail);
-    content = (
-      <React.Fragment>
-        <img
-          className={clsx({ [classes.imageLoading]: loading })}
-          src={thumbnailUrl}
-          onLoad={() => setLoading(false)} />
-        {loading && (
-          <icons.documents.Pending className={classes.imageFallback} />
-        )}
-      </React.Fragment>
-    );
-  } else {
-    content = (
-      <icons.documents.None className={classes.imageFallback} />
-    );
-  }
+NotNull.propTypes = {
+  thumbnail: PropTypes.string.isRequired
+};
 
+const ThumbnailImage = ({ document, className, ...props }) => {
+  const classes = useCommonStyles();
   return (
     <div className={clsx(classes.container, className)} {...props}>
-      {content}
+      {document.thumbnail ? (
+        <NotNull thumbnail={document.thumbnail}/>
+      ) : (
+        <icons.documents.None className={classes.imageFallback} />
+      )}
     </div>
   );
 };
