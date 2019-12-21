fb0fb34687945bb9ef0edfc8a9ea79fb335ecb20
vincent-tr <vincent.tr@gmail.com>
2019-12-21 19:09:05 +0100

    leaflet cleanup

--- packages/mylife-gallery/public/src/common/document-viewer/image/detail-location.js
+++ packages/mylife-gallery/public/src/common/document-viewer/image/detail-location.js
@@ -1,19 +1,8 @@
 'use strict';
 
 import { React, PropTypes, mui } from 'mylife-tools-ui';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-
-// https://github.com/PaulLeCam/react-leaflet/issues/453
-import L from 'leaflet';
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-});
+import { Map, Marker, TileLayer } from 'react-leaflet';
+import './leaflet-setup';
 
 const DetailLocation = ({ location }) => {
   if(!location) {
---
+++ packages/mylife-gallery/public/src/common/document-viewer/image/leaflet-setup.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import 'leaflet/dist/leaflet.css';
+// https://github.com/PaulLeCam/react-leaflet/issues/453
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
