3fc895e206b5efcb2900158d36d09ce383393f73
vincent-tr <vincent.tr@gmail.com>
2019-12-21 12:24:01 +0100

    fix marker

--- packages/mylife-gallery/public/src/common/document-viewer/image/detail-location.js
+++ packages/mylife-gallery/public/src/common/document-viewer/image/detail-location.js
@@ -4,6 +4,17 @@ import { React, PropTypes, mui } from 'mylife-tools-ui';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
+// https://github.com/PaulLeCam/react-leaflet/issues/453
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
+
 const DetailLocation = ({ location }) => {
   if(!location) {
     return null;
@@ -24,9 +35,7 @@ const DetailLocation = ({ location }) => {
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             />
-            <Marker position={position}>
-              <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-            </Marker>
+            <Marker position={position} />
           </Map>
         } />
     </mui.ListItem>
