a769af7d4d8ac565d2e8809efcd060ec831a4ba7
vincent-tr <vincent@stoic.com>
2020-01-03 17:48:16 +0100

    getSuggestion selector

--- packages/mylife-gallery/public/src/suggestions/selectors.js
+++ packages/mylife-gallery/public/src/suggestions/selectors.js
@@ -1,7 +1,12 @@
 'use strict';
 
-import { io } from 'mylife-tools-ui';
+import { io, createSelector } from 'mylife-tools-ui';
 
 const getStats = state => state.suggestion;
 export const getViewId = state => getStats(state).viewId;
-export const getView = state => io.getView(state, getViewId(state));
+const getView = state => io.getView(state, getViewId(state));
+
+export const getSuggestions = createSelector(
+  [ getView ],
+  (view) => view.valueSeq().toArray()
+);
