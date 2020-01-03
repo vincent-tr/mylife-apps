fe0e02d0bb485256081128e9bc36b41b16055f11
vincent-tr <vincent@stoic.com>
2020-01-03 18:27:51 +0100

    sort suggestions

--- packages/mylife-gallery/public/src/suggestions/selectors.js
+++ packages/mylife-gallery/public/src/suggestions/selectors.js
@@ -8,5 +8,21 @@ const getView = state => io.getView(state, getViewId(state));
 
 export const getSuggestions = createSelector(
   [ getView ],
-  (view) => view.valueSeq().toArray()
+  (view) => view.valueSeq().sort(suggestionComparer).toArray()
 );
+
+const TYPE_ORDER = {
+  'warn-syncing': 0,
+  'clean-others': 1,
+  'clean-duplicates': 2,
+  'album-creation': 3,
+};
+
+function suggestionComparer(sug1, sug2) {
+  // sort by type
+  if(sug1.type === sug2.type) {
+    return sug1._id < sug2._id ? 1 : -1;
+  }
+
+  return TYPE_ORDER[sug1.type] - TYPE_ORDER[sug2.type];
+}
