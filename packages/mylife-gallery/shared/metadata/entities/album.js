b93919496855c009a7fee050cb3cd9211dae118c
vincent-tr <vincent@stoic.com>
2020-01-03 14:39:10 +0100

    better metadata

--- packages/mylife-gallery/shared/metadata/entities/album.js
+++ packages/mylife-gallery/shared/metadata/entities/album.js
@@ -8,8 +8,8 @@ module.exports = {
     { id: 'title', name: 'Titre', datatype: 'name', constraints: ['not-null', 'not-empty'] },
     { id: 'caption', name: 'Légende', datatype: 'text' },
     { id: 'documents', name: 'Documents', datatype: 'list:document-reference' },
-    { id: 'keywords', name: 'Mots clés', datatype: 'list:name' },
-    { id: 'thumbnails', name: 'Miniatures', datatype: 'list:identifier' }, // we do not directly reference thumbnail because it is not loaded as store collection
+    { id: 'keywords', name: 'Mots clés', datatype: 'list:name', constraints: ['not-null'], initial: [] },
+    { id: 'thumbnails', name: 'Miniatures', datatype: 'list:identifier', constraints: ['not-null'], initial: [] }, // we do not directly reference thumbnail because it is not loaded as store collection
   ],
   display: obj => obj.title
 };
