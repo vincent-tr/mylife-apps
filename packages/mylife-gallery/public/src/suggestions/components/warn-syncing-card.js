743fe3d498ee6f1032a8adf294af365951393929
vincent-tr <vincent@stoic.com>
2020-01-08 15:31:18 +0100

    adapt sync warn

--- packages/mylife-gallery/public/src/suggestions/components/warn-syncing-card.js
+++ packages/mylife-gallery/public/src/suggestions/components/warn-syncing-card.js
@@ -1,13 +1,34 @@
 'use strict';
 
-import { React, PropTypes, mui } from 'mylife-tools-ui';
+import { React, PropTypes, mui, useState, useInterval, useLifecycle } from 'mylife-tools-ui';
 import humanizeDuration from 'humanize-duration';
 
+const useStyles = mui.makeStyles(theme => ({
+  icon: {
+    color: theme.status.warning,
+    marginRight: theme.spacing(2)
+  }
+}));
+
 const WarnSyncingCard = ({ definition, ...props }) => {
-  const delay = humanizeDuration(definition.delay, { language: 'fr', round: true });
+  const classes = useStyles();
+  const [delay, setDelay] = useState(0);
+  const compute = () => setDelay(computeDelay(definition));
+
+  useInterval(compute, 500);
+  useLifecycle(compute);
+
   return (
     <mui.Card {...props}>
-      <mui.CardHeader title={'Synchronisation en cours'} subheader={`Dernier document intégré il y a ${delay}`} />
+      <mui.CardHeader
+        title={
+          <>
+            <mui.icons.Warning className={classes.icon} />
+            Synchronisation en cours
+          </>
+        }
+        subheader={`Dernier document intégré il y a ${delay}`}
+      />
       <mui.CardContent>
         <mui.Typography color='textSecondary'>
           {'Vous devriez attendre la fin de la synchronisation avant d\'utiliser les suggestions'}
@@ -22,3 +43,8 @@ WarnSyncingCard.propTypes = {
 };
 
 export default WarnSyncingCard;
+
+function computeDelay(definition) {
+  const delay = Date.now() - definition.lastIntegration;
+  return humanizeDuration(delay, { language: 'fr', round: true });
+}
