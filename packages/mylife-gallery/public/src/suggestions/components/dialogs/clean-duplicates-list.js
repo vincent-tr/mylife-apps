d4115c3ef6cf40c0bf342c38c5efac4b2c266682
vincent-tr <vincent@stoic.com>
2020-01-09 17:05:48 +0100

    wip selection list

--- packages/mylife-gallery/public/src/suggestions/components/dialogs/clean-duplicates-list.js
+++ packages/mylife-gallery/public/src/suggestions/components/dialogs/clean-duplicates-list.js
@@ -1,11 +1,36 @@
 'use strict';
 
-import { React, PropTypes, mui } from 'mylife-tools-ui';
+import { React, PropTypes, mui, Virtuoso } from 'mylife-tools-ui';
+
+const ListItemContent = ({ document, selection, setSelection }) => (
+  <mui.ListItemText>
+    {document.paths[0].path}
+  </mui.ListItemText>
+);
 
 const CleanDuplicatesList = ({ documents, selection, setSelection, ...props }) => {
 
   return (
-    <div {...props} />
+    <Virtuoso
+      ListContainer={({ listRef, className, style, children }) => (
+        <mui.List
+          ref={listRef}
+          style={{ margin: 0, padding: 0, ...style }}
+          className={className}
+        >
+          {children}
+        </mui.List>
+      )}
+      ItemContainer={({ children, ...props }) => (
+        <mui.ListItem {...props} style={{ margin: 0 }}>
+          {children}
+        </mui.ListItem>
+      )}
+      totalCount={documents.length}
+      item={index => (<ListItemContent document={documents[index]} selection={selection} setSelection={setSelection} />)}
+      style={{ height: '100%', width: '100%', overflowX: 'hidden' }}
+      {...props}
+    />
   );
 };
 
