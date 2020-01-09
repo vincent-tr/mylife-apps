89322c319eadc5db629ad3d6ec79a48984ada6be
vincent-tr <vincent@stoic.com>
2020-01-09 17:14:46 +0100

    wip clean duplicates

--- packages/mylife-gallery/public/src/suggestions/components/dialogs/clean-duplicates-list.js
+++ packages/mylife-gallery/public/src/suggestions/components/dialogs/clean-duplicates-list.js
@@ -2,11 +2,38 @@
 
 import { React, PropTypes, mui, Virtuoso } from 'mylife-tools-ui';
 
-const ListItemContent = ({ document, selection, setSelection }) => (
-  <mui.ListItemText>
-    {document.paths[0].path}
-  </mui.ListItemText>
-);
+const ListItemContent = ({ document, selection, setSelection }) => {
+  const paths = document.paths.map(fsItem => fsItem.path);
+  const isChecked = (index) => selection.get(document._id) === index;
+  const toogleChecked = (index) => {
+    if(isChecked(index)) {
+      setSelection(selection.delete(document._id));
+    } else {
+      setSelection(selection.set(document._id, index));
+    }
+  };
+
+  return (
+    <mui.ListItemText>
+      <mui.FormGroup row>
+        {paths.map((path, index) => (
+          <mui.FormControlLabel key={index}
+            control={
+              <mui.Checkbox checked={isChecked(index)} onChange={() => toogleChecked(index)}  />
+            }
+            label={path}
+          />
+        ))}
+      </mui.FormGroup>
+    </mui.ListItemText>
+  );
+};
+
+ListItemContent.propTypes = {
+  document: PropTypes.object.isRequired,
+  selection: PropTypes.object.isRequired,
+  setSelection: PropTypes.func.isRequired
+};
 
 const CleanDuplicatesList = ({ documents, selection, setSelection, ...props }) => (
   <Virtuoso
