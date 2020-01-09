6bf085397b3e5282cc8a7c48526f5884611d1e70
vincent-tr <vincent@stoic.com>
2020-01-09 11:37:56 +0100

    stepper button layout

--- packages/mylife-tools-ui/src/components/stepper/stepper-actions.js
+++ packages/mylife-tools-ui/src/components/stepper/stepper-actions.js
@@ -2,19 +2,32 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from '@material-ui/core';
+import { Button, makeStyles } from '@material-ui/core';
+import clsx from 'clsx';
 
-const StepperActions = ({ canPrev, canNext, canSkip, canFinish, canCancel, onAction, ...props }) => (
-  <div {...props}>
-    <Button disabled={!canPrev} onClick={() => onAction('prev')}>{'Précédent'}</Button>
-    <Button disabled={!canNext} onClick={() => onAction('next')}>{'Suivant'}</Button>
-    {canSkip && (<Button onClick={() => onAction('skip')}>{'Sauter'}</Button>)}
-    {canFinish && (<Button onClick={() => onAction('finish')}>{'Terminer'}</Button>)}
-    {canCancel && (<Button onClick={() => onAction('cancel')}>{'Annuler'}</Button>)}
-  </div>
-);
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+const StepperActions = ({ className, canPrev, canNext, canSkip, canFinish, canCancel, onAction, ...props }) => {
+  const classes = useStyles();
+  return (
+    <div className={clsx(className, classes.root)} {...props} >
+      <Button disabled={!canPrev} onClick={() => onAction('prev')} variant='contained'>{'Précédent'}</Button>
+      <Button disabled={!canNext} onClick={() => onAction('next')} variant='contained' color='primary'>{'Suivant'}</Button>
+      {canSkip && (<Button onClick={() => onAction('skip')} variant='contained'>{'Sauter'}</Button>)}
+      {canFinish && (<Button onClick={() => onAction('finish')} variant='contained' color='primary'>{'Terminer'}</Button>)}
+      {canCancel && (<Button onClick={() => onAction('cancel')} variant='contained'>{'Annuler'}</Button>)}
+    </div>
+  );
+};
 
 StepperActions.propTypes = {
+  className: PropTypes.string,
   canPrev: PropTypes.bool.isRequired,
   canNext: PropTypes.bool.isRequired,
   canSkip: PropTypes.bool.isRequired,
