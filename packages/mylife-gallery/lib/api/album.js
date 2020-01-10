5970ee0b246c743d05ddc362055e37f63095ca24
vincent-tr <vincent@stoic.com>
2020-01-10 12:37:09 +0100

    album API

--- packages/mylife-gallery/lib/api/album.js
+++ packages/mylife-gallery/lib/api/album.js
@@ -6,3 +6,22 @@ const { base } = require('./decorators');
 exports.meta = {
   name : 'album'
 };
+
+exports.notifyAlbums = [ base, (session/*, message*/) => {
+  return business.albumsNotify(session);
+} ];
+
+exports.createAlbum = [ base, (session, message) => {
+  const { values } = message;
+  return business.albumCreate(values);
+} ];
+
+exports.addDocumentToAlbum = [ base, (session, message) => {
+  const { id, reference } = message;
+  return business.albumAddDocument(id, reference);
+} ];
+
+exports.removeDocumentFromAlbum = [ base, (session, message) => {
+  const { id, reference } = message;
+  return business.albumRemoveDocument(id, reference);
+} ];
--- packages/mylife-gallery/lib/business/album.js
+++ packages/mylife-gallery/lib/business/album.js
@@ -1,24 +1,76 @@
 'use strict';
 
-const { createLogger, getStoreCollection, getMetadataEntity } = require('mylife-tools-server');
+const { createLogger, getStoreCollection, getMetadataEntity, notifyView } = require('mylife-tools-server');
 const { utils } = require('mylife-tools-common');
+const business = require('.');
+
 const logger = createLogger('mylife:gallery:business:album');
 
-exports.albumList = albumList;
-function albumList() {
-  return []; // TODO
-}
+exports.albumList = () => {
+  const albums = getStoreCollection('albums');
+  return albums.list();
+};
 
 exports.albumGet = (id) => {
-  throw new Error('TODO');
+  const albums = getStoreCollection('albums');
+  return albums.get(id);
 };
 
 exports.albumCreate = (values) => {
   const entity = getMetadataEntity('album');
-  const collection = getStoreCollection('albums');
+  const albums = getStoreCollection('albums');
   const newAlbum = entity.newObject(values);
 
-  const item = collection.set(newAlbum);
+  const item = albums.set(newAlbum);
   logger.info(`Created album '${item._id}'`);
   return item;
 };
+
+exports.albumAddDocument = (albumId, reference) => {
+  const entity = getMetadataEntity('album');
+  const albums = getStoreCollection('albums');
+  const album = albums.get(albumId);
+
+  // ensure reference is valid
+  business.documentGet(reference.type, reference.id);
+
+  const index = findDocRefIndex(album, reference);
+  if(index !== -1) {
+    throw new Error('Le document existe déjà');
+  }
+
+  const newDocuments = utils.immutable.arrayPush(album.documents, reference);
+  const newAlbum = entity.getField('documents').setValue(newDocuments);
+
+  logger.info(`Adding document '${reference.type}:${reference.id}' on album '${album._id}'`);
+  albums.set(newAlbum);
+};
+
+exports.albumRemoveDocument = (albumId, reference) => {
+  const entity = getMetadataEntity('album');
+  const albums = getStoreCollection('albums');
+  const album = albums.get(albumId);
+  const index = findDocRefIndex(album, reference);
+  if(index === -1) {
+    throw new Error('Le document n\'existe pas');
+  }
+
+  const newDocuments = utils.immutable.arrayRemove(album.documents, index);
+  const newAlbum = entity.getField('documents').setValue(newDocuments);
+
+  logger.info(`Removing document '${reference.type}:${reference.id}' on album '${album._id}'`);
+  albums.set(newAlbum);
+};
+
+exports.albumsNotify = (session) => {
+  const albums = getStoreCollection('albums');
+  return notifyView(session, albums.createView());
+};
+
+function docRefEquals(docref1, docref2) {
+  return docref1.type === docref2.type && docref1.id === docref2.id;
+}
+
+function findDocRefIndex(album, reference) {
+  return album.documents.findIndex(docref => docRefEquals(docref, reference));
+}
