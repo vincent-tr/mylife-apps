96e496a60dde39f9e23039516ef72acbabb45bfd
vincent-tr <vincent@stoic.com>
2020-01-16 17:40:00 +0100

    wip slideshow skeleton

--- packages/mylife-gallery/public/src/reducers.js
+++ packages/mylife-gallery/public/src/reducers.js
@@ -5,5 +5,6 @@ export { default as browse } from './browse/reducer';
 export { default as stats } from './stats/reducer';
 export { default as suggestion } from './suggestions/reducer';
 export { default as album } from './album/reducer';
+export { default as slideshows } from './slideshows/reducer';
 export { default as documentViewer } from './document-viewer/reducer';
 export { default as commonAlbumView } from './common/album-view/reducer';
--- packages/mylife-gallery/public/src/slideshows/reducer.js
+++ packages/mylife-gallery/public/src/slideshows/reducer.js
@@ -3,31 +3,11 @@
 import { handleActions, io } from 'mylife-tools-ui';
 import actionTypes from './action-types';
 
-const initialCriteria = {
-  title: null,
-  keywords: null,
-};
-
-const initialDisplay = {
-  sortField: 'title',
-  sortOrder: 'desc'
-};
-
 export default handleActions({
 
   [actionTypes.SET_VIEW] : (state, action) => ({
     ...state,
     viewId: action.payload
-  }),
-
-  [actionTypes.SET_DISPLAY] : (state, action) => ({
-    ...state,
-    display: action.payload || initialDisplay
-  }),
-
-  [actionTypes.SET_CRITERIA] : (state, action) => ({
-    ...state,
-    criteria: action.payload || initialCriteria
   }),
 
   [io.actionTypes.SET_ONLINE] : (state) => ({
@@ -37,6 +17,4 @@ export default handleActions({
 
 }, {
   viewId: null,
-  criteria: initialCriteria,
-  display: initialDisplay
 });
