9b81c52eb635a30a5f3278f9bc7fe2311d11adeb
vincent-tr <vincent.tr@gmail.com>
2020-01-19 10:20:26 +0100

    add video marker

--- packages/mylife-gallery/public/src/common/document-thumbnail-list.js
+++ packages/mylife-gallery/public/src/common/document-thumbnail-list.js
@@ -1,9 +1,10 @@
 'use strict';
 
-import { React, PropTypes } from 'mylife-tools-ui';
+import { React, PropTypes, mui } from 'mylife-tools-ui';
 import * as documentViewer from '../document-viewer';
 import { ThumbnailDocument } from './thumbnail';
 import ThumbnailList from './thumbnail-list';
+import icons from './icons';
 
 const DocumentThumbnailList = ({ data, ...props }) => (
   <ThumbnailList data={data} getTileInfo={getTileInfo} {...props} />
@@ -16,11 +17,50 @@ DocumentThumbnailList.propTypes = {
 
 export default DocumentThumbnailList;
 
+const useTypeMarkerStyles = mui.makeStyles({
+  container: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    height: 48,
+    width: 48,
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+    color: 'rgba(0, 0, 0, 0.5)'
+  }
+});
+
+const TypeMarker = ({ document }) => {
+  const classes = useTypeMarkerStyles();
+
+  const type = document._entity;
+  if(type !== 'video') {
+    return null;
+  }
+
+  return (
+    <div className={classes.container}>
+      <icons.documents.Video className={classes.image} />
+    </div>
+  );
+};
+
+TypeMarker.propTypes = {
+  document: PropTypes.object.isRequired
+};
+
 function getTileInfo(data, index) {
   const { document, info } = data[index];
   const { title, subtitle } = info;
   const onClick = () => showViewer(data, index);
-  const thumbnail = (<ThumbnailDocument document={document} />);
+  const thumbnail = (
+    <>
+      <TypeMarker document={document} />
+      <ThumbnailDocument document={document} />
+    </>
+  );
 
   return { title, subtitle, thumbnail, onClick };
 }
