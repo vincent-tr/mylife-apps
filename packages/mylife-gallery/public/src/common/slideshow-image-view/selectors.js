dbfe3b33ce4f457192a68fb3ef6e75b25a259333
vincent-tr <vincent@stoic.com>
2020-01-21 17:41:27 +0100

    implement selector

--- packages/mylife-gallery/public/src/common/slideshow-image-view/selectors.js
+++ packages/mylife-gallery/public/src/common/slideshow-image-view/selectors.js
@@ -8,11 +8,28 @@ export const getSlideshowImageView = state => io.getView(state, getViewId(state)
 
 const getImagesBySlideshow = createSelector(
   [ getSlideshowImageView ],
-  (view) => new immutable.Map().withMutations(map => {
-    // TODO
-  })
+  (view) => {
+    const map = new Map();
+    for(const item of view.valueSeq()) {
+      let list = map.get(item.slideshow);
+      if(!list) {
+        list = [];
+        map.set(item.slideshow, list);
+      }
+      list.push(item);
+    }
+
+    for(const [key, list] of map) {
+      list.sort((it1, it2) => it1.index - it2.index);
+      map.set(key, new immutable.List(list));
+    }
+
+    return new immutable.Map(map);
+  }
 );
 
-export const getSlideshowImages = (state, slideshowId) => getImagesBySlideshow(state).get(slideshowId);
+const emptyList = new immutable.List();
+
+export const getSlideshowImages = (state, slideshowId) => getImagesBySlideshow(state).get(slideshowId) || emptyList;
 
 export const getRefs = state => getBase(state).refs;
