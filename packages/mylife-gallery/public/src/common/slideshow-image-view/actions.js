ec063ad87c87c7962cfe942fb8ecbfc77bbf4c51
vincent-tr <vincent@stoic.com>
2020-01-21 20:30:46 +0100

    fix ref/unref

--- packages/mylife-gallery/public/src/common/slideshow-image-view/actions.js
+++ packages/mylife-gallery/public/src/common/slideshow-image-view/actions.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import { Mutex } from 'async-mutex';
 import { createAction } from 'mylife-tools-ui';
 import { createOrUpdateView, deleteView } from '../action-tools';
 import actionTypes from './action-types';
@@ -24,27 +25,38 @@ const clearSlideshowImages = () => deleteView({
   setViewAction: local.setView
 });
 
-export const refSlideshowImageView = (slideshowId) => async (dispatch, getState) => {
-  const wasRef = isRef(getState(), slideshowId);
-  dispatch(local.ref(slideshowId));
+// needed  because fetchSlideshowsImages/clearSlideshowImages is not atomic
+const mutex = new Mutex();
 
-  // first ref to this slideshow, need to fetch it
-  if(!wasRef) {
-    await dispatch(fetchSlideshowsImages());
-  }
+export const refSlideshowImageView = (slideshowId) => async (dispatch, getState) => {
+  await mutex.runExclusive(async () => {
+
+    const wasRef = isRef(getState(), slideshowId);
+    dispatch(local.ref(slideshowId));
+
+    // first ref to this slideshow, need to fetch it
+    if(!wasRef) {
+      await dispatch(fetchSlideshowsImages());
+    }
+
+  });
 };
 
 export const unrefSlideshowImageView = (slideshowId) => async (dispatch, getState) => {
-  dispatch(local.unref());
-  const state = getState();
+  await mutex.runExclusive(async () => {
 
-  if(!isAnyRef(state)) {
-    // no ref amymore, release view
-    await dispatch(clearSlideshowImages());
-  } else if(!isRef(state, slideshowId)) {
-    // not ref to this slideshow anymore, fetch view without this one
-    await dispatch(fetchSlideshowsImages());
-  }
+    dispatch(local.unref(slideshowId));
+    const state = getState();
+
+    if(!isAnyRef(state)) {
+      // no ref amymore, release view
+      await dispatch(clearSlideshowImages());
+    } else if(!isRef(state, slideshowId)) {
+      // not ref to this slideshow anymore, fetch view without this one
+      await dispatch(fetchSlideshowsImages());
+    }
+    
+  });
 };
 
 function getCriteriaFromState(state) {
