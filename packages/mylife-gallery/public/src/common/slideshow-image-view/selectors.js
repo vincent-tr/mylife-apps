aff329d1ceb1a9b11e887f63a49f5440cd752575
vincent-tr <vincent@stoic.com>
2020-01-21 15:15:44 +0100

    wip

--- packages/mylife-gallery/public/src/common/slideshow-image-view/selectors.js
+++ packages/mylife-gallery/public/src/common/slideshow-image-view/selectors.js
@@ -1,9 +1,18 @@
 'use strict';
 
-import { io } from 'mylife-tools-ui';
+import { io, createSelector, immutable } from 'mylife-tools-ui';
 
 const getBase = state => state.commonSlideshowImageView;
 export const getViewId = state => getBase(state).viewId;
 export const getSlideshowImageView = state => io.getView(state, getViewId(state));
+
+const getImagesBySlideshow = createSelector(
+  [ getSlideshowImageView ],
+  (view) => new immutable.Map().withMutations(map => {
+    // TODO
+  })
+);
+
+export const getSlideshowImages = (state, slideshowId) => getImagesBySlideshow(state).get(slideshowId);
 
 export const getRefs = state => getBase(state).refs;
