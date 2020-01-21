620f07f7566bac626340c76540748bf432b7931e
vincent-tr <vincent@stoic.com>
2020-01-21 14:18:31 +0100

    common detail list

--- packages/mylife-gallery/public/src/document-viewer/components/common/detail-albums.js
+++ packages/mylife-gallery/public/src/document-viewer/components/common/detail-albums.js
@@ -2,7 +2,7 @@
 
 import { React, PropTypes, useMemo, mui, useDispatch, dialogs } from 'mylife-tools-ui';
 import { useAlbumView } from '../../../common/album-view';
-import { renderObject } from '../../../common/metadata-utils';
+import DetailList from './detail-list';
 import { addDocumentToAlbum, removeDocumentFromAlbum, createAlbumWithDocument } from '../../actions';
 
 const useConnect = () => {
@@ -14,84 +14,7 @@ const useConnect = () => {
   }), [dispatch]);
 };
 
-const useStyles = mui.makeStyles(theme => ({
-  list: {
-    width: 300,
-    height: 180,
-    overflow: 'auto'
-  },
-  addButton: {
-    marginLeft: theme.spacing(1),
-    color: theme.palette.success.main
-  },
-  deleteButton: {
-    marginLeft: theme.spacing(1),
-    color: theme.palette.error.main
-  }
-}));
-
-const useAddButtonStyles = mui.makeStyles(theme => ({
-  listAddIcon: {
-    marginRight: theme.spacing(1),
-    color: theme.palette.success.main
-  }
-}));
-
-const AddButton = ({ albums, addAlbum, createAlbum, ...props }) => {
-  const classes = useAddButtonStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const onOpen = event => setAnchorEl(event.currentTarget);
-  const onClose = () => setAnchorEl(null);
-
-  const onAdd = (album) => {
-    onClose();
-    addAlbum(album);
-  };
-
-  const onNew = async () => {
-    onClose();
-
-    const { result, text: title } = await dialogs.input({ title: 'Titre du nouvel album', label: 'Titre' });
-    if(result !== 'ok') {
-      return;
-    }
-
-    createAlbum(title);
-  };
-
-  return (
-    <>
-    <mui.Tooltip title='Ajouter le document à un album'>
-      <mui.IconButton {...props} onClick={onOpen}>
-        <mui.icons.AddCircle />
-      </mui.IconButton>
-    </mui.Tooltip>
-    <mui.Menu
-      anchorEl={anchorEl}
-      open={!!anchorEl}
-      onClose={onClose}
-    >
-      <mui.MenuItem onClick={onNew}>
-        <mui.icons.AddCircle className={classes.listAddIcon}/>
-        Nouvel album ...
-      </mui.MenuItem>
-      {albums.map(album => (
-        <mui.MenuItem key={album._id} onClick={() => onAdd(album)}>{renderObject(album)}</mui.MenuItem>
-      ))}
-    </mui.Menu>
-    </>
-  );
-};
-
-AddButton.propTypes = {
-  albums: PropTypes.array.isRequired,
-  addAlbum: PropTypes.func.isRequired,
-  createAlbum: PropTypes.func.isRequired
-};
-
 const DetailAlbums = ({ documentWithInfo }) => {
-  const classes = useStyles();
   const { albums, albumView } = useAlbumView();
   const { addAlbum, removeAlbum, createAlbum } = useConnect();
 
@@ -104,46 +27,27 @@ const DetailAlbums = ({ documentWithInfo }) => {
     return albums.filter(album => !idSet.has(album._id));
   }, [albumIds, albums]);
 
-  return (
-    <mui.ListItem>
-      <mui.ListItemText
-        disableTypography
-        primary={
-          <mui.Typography>
-            {'Albums'}
-            <AddButton
-              albums={addableAlbums}
-              addAlbum={album => addAlbum(document, album)}
-              createAlbum={title => createAlbum(document, title)}
-              className={classes.addButton}
-            />
-          </mui.Typography>
-        }
-        secondary={
-          <mui.List className={classes.list} dense>
-            {albumIds.map(id => {
-              const album = albumView.get(id);
-              if(!album) { // albumView not ready
-                return null;
-              }
+  const onNew = async () => {
+    const { result, text: title } = await dialogs.input({ title: 'Titre du nouvel album', label: 'Titre' });
+    if(result !== 'ok') {
+      return;
+    }
 
-              return (
-                <mui.ListItem key={id}>
-                  <mui.Typography>
-                    {album.title}
-                  </mui.Typography>
-                  <mui.Tooltip title={'Enlever le document de l\'album'}>
-                    <mui.IconButton className={classes.deleteButton} onClick={() => removeAlbum(document, album)}>
-                      <mui.icons.Delete />
-                    </mui.IconButton>
-                  </mui.Tooltip>
-                </mui.ListItem>
-              );
-            })}
-          </mui.List>
-        }
-      />
-    </mui.ListItem>
+    createAlbum(title);
+  };
+
+  return (
+    <DetailList
+      title={'Albums'}
+      addTooltip={'Ajouter le document à un album'}
+      newTooltip={'Nouvel album ...'}
+      deleteTooltip={'Enlever le document de l\'album'}
+      onAdd={album => addAlbum(document, album)}
+      onNew={onNew}
+      onDelete={album => removeAlbum(document, album)}
+      items={albumIds.map(id => albumView.get(id))}
+      addableItems={addableAlbums}
+    />
   );
 };
 
---
+++ packages/mylife-gallery/public/src/document-viewer/components/common/detail-list.js
@@ -0,0 +1,135 @@
+'use strict';
+
+import { React, PropTypes, useMemo, mui } from 'mylife-tools-ui';
+import { renderObject } from '../../../common/metadata-utils';
+
+const useStyles = mui.makeStyles(theme => ({
+  list: {
+    width: 300,
+    height: 180,
+    overflow: 'auto',
+    borderWidth: 1,
+    borderColor: mui.colors.grey[300],
+    borderStyle: 'solid',
+  },
+  addButton: {
+    marginLeft: theme.spacing(1),
+    color: theme.palette.success.main
+  },
+  deleteButton: {
+    marginLeft: theme.spacing(1),
+    color: theme.palette.error.main
+  }
+}));
+
+const useAddButtonStyles = mui.makeStyles(theme => ({
+  listAddIcon: {
+    marginRight: theme.spacing(1),
+    color: theme.palette.success.main
+  }
+}));
+
+const AddButton = ({ addTooltip, newTooltip, items, onAdd, onNew, ...props }) => {
+  const classes = useAddButtonStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const onOpen = event => setAnchorEl(event.currentTarget);
+  const onClose = () => setAnchorEl(null);
+
+  const handleAdd = (item) => {
+    onClose();
+    onAdd(item);
+  };
+
+  return (
+    <>
+    <mui.Tooltip title={addTooltip}>
+      <mui.IconButton {...props} onClick={onOpen}>
+        <mui.icons.AddCircle />
+      </mui.IconButton>
+    </mui.Tooltip>
+    <mui.Menu
+      anchorEl={anchorEl}
+      open={!!anchorEl}
+      onClose={onClose}
+    >
+      <mui.MenuItem onClick={onNew}>
+        <mui.icons.AddCircle className={classes.listAddIcon}/>
+        {newTooltip}
+      </mui.MenuItem>
+      {items.map(item => (
+        <mui.MenuItem key={item._id} onClick={() => handleAdd(item)}>{renderObject(item)}</mui.MenuItem>
+      ))}
+    </mui.Menu>
+    </>
+  );
+};
+
+AddButton.propTypes = {
+  addTooltip: PropTypes.string.isRequired,
+  newTooltip: PropTypes.string.isRequired,
+  items: PropTypes.array.isRequired,
+  onAdd: PropTypes.func.isRequired,
+  onNew: PropTypes.func.isRequired
+};
+
+const DetailList = ({ title, addTooltip, newTooltip, deleteTooltip, onAdd, onNew, onDelete, items, addableItems }) => {
+  const classes = useStyles();
+
+  return (
+    <mui.ListItem>
+      <mui.ListItemText
+        disableTypography
+        primary={
+          <mui.Typography>
+            {title}
+            <AddButton
+              addTooltip={addTooltip}
+              newTooltip={newTooltip}
+              items={addableItems}
+              onAdd={item => onAdd(item)}
+              onNew={onNew}
+              className={classes.addButton}
+            />
+          </mui.Typography>
+        }
+        secondary={
+          <mui.List className={classes.list} dense>
+            {items.map(item => {
+              if(!item) { // can happen if item view not ready
+                return null;
+              }
+
+              return (
+                <mui.ListItem key={item._id}>
+                  <mui.Typography>
+                    {renderObject(item)}
+                  </mui.Typography>
+                  <mui.Tooltip title={deleteTooltip}>
+                    <mui.IconButton className={classes.deleteButton} onClick={() => onDelete(item)}>
+                      <mui.icons.Delete />
+                    </mui.IconButton>
+                  </mui.Tooltip>
+                </mui.ListItem>
+              );
+            })}
+          </mui.List>
+        }
+      />
+    </mui.ListItem>
+  );
+};
+
+DetailList.propTypes = {
+  title: PropTypes.string.isRequired,
+  addTooltip: PropTypes.string.isRequired,
+  newTooltip: PropTypes.string.isRequired,
+  deleteTooltip: PropTypes.string.isRequired,
+  onAdd: PropTypes.func.isRequired,
+  onNew: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
+  items: PropTypes.array.isRequired,
+  addableItems: PropTypes.array.isRequired
+};
+
+export default DetailList;
