27a7ab9d048d958036377e19a8630ec002f3357f
vincent-tr <vincent@stoic.com>
2020-01-22 11:35:01 +0100

    album ordering in slideshow

--- packages/mylife-gallery/public/src/slideshows/components/album-list.js
+++ packages/mylife-gallery/public/src/slideshows/components/album-list.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import { React, PropTypes, mui, useDispatch, useMemo } from 'mylife-tools-ui';
 import { useAlbumView } from '../../common/album-view';
 import { renderObject } from '../../common/metadata-utils';
@@ -14,41 +15,67 @@ const useConnect = () => {
 };
 
 const useStyles = mui.makeStyles(theme => ({
+  listItem: {
+    cursor: 'grab'
+  },
   deleteButton: {
     marginLeft: theme.spacing(1),
     color: theme.palette.error.main
   }
 }));
 
-const AlbumList = ({ slideshow, ...props }) => {
+const Item = ({ album, onDelete }) => {
   const classes = useStyles();
+  return (
+    <mui.ListItem className={classes.listItem}>
+      <mui.ListItemText primary={renderObject(album)} />
+      <mui.ListItemSecondaryAction>
+        <mui.Tooltip title={'Enlever l\'album du diaporama'}>
+          <mui.IconButton className={classes.deleteButton} onClick={() => onDelete(album)}>
+            <mui.icons.Delete />
+          </mui.IconButton>
+        </mui.Tooltip>
+      </mui.ListItemSecondaryAction>
+    </mui.ListItem>
+  );
+};
+
+Item.propTypes = {
+  album: PropTypes.object.isRequired,
+  onDelete: PropTypes.func.isRequired
+};
+
+const SortableItem = SortableElement(Item);
+
+const List = ({ albums, onDelete, ...props }) => (
+  <mui.List {...props}>
+    {albums.map((album, index) => (
+      <SortableItem key={album._id} index={index} album={album} onDelete={onDelete} />
+    ))}
+  </mui.List>
+);
+
+List.propTypes = {
+  albums: PropTypes.array.isRequired,
+  onDelete: PropTypes.func.isRequired
+};
+
+const SortableList = SortableContainer(List);
+
+const AlbumList = ({ slideshow, ...props }) => {
   const { albumView } = useAlbumView();
   const { removeAlbumFromSlideshow, moveAlbumInSlideshow } = useConnect();
   const albumIds = slideshow.albums;
+  const albums = albumIds.map(id => albumView.get(id)).filter(album => album);
 
   return (
-    <mui.List dense {...props}>
-      {albumIds.map(id => {
-        const album = albumView.get(id);
-        if(!album) { // albumView not ready
-          return null;
-        }
-
-        return (
-          <mui.ListItem key={id}>
-            <mui.ListItemText primary={renderObject(album)} />
-            <mui.ListItemSecondaryAction>
-              <mui.Tooltip title={'Enlever l\'album du diaporama'}>
-                <mui.IconButton className={classes.deleteButton} onClick={() => removeAlbumFromSlideshow(slideshow, album)}>
-                  <mui.icons.Delete />
-                </mui.IconButton>
-              </mui.Tooltip>
-              {/* TODO: move => https://github.com/clauderic/react-sortable-hoc */}
-            </mui.ListItemSecondaryAction>
-          </mui.ListItem>
-        );
-      })}
-    </mui.List>
+    <SortableList
+      dense
+      albums={albums}
+      onDelete={album => removeAlbumFromSlideshow(slideshow, album)}
+      onSortEnd={({ oldIndex, newIndex }) => moveAlbumInSlideshow(slideshow, oldIndex, newIndex)}
+      {...props}
+    />
   );
 };
 
