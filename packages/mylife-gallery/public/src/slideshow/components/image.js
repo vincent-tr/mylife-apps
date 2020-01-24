75671df6080caeeb2928acc07a5ae6185b67fe71
vincent-tr <vincent@stoic.com>
2020-01-24 12:07:53 +0100

    wip transitions

--- packages/mylife-gallery/public/src/slideshow/components/image.js
+++ packages/mylife-gallery/public/src/slideshow/components/image.js
@@ -10,11 +10,43 @@ const useStyles = mui.makeStyles({
   },
 });
 
-const Image = ({ slideshow, url, className, ...props }) => {
-  // TODO: transition
+const ImageContent = ({ url, className, ...props }) => {
   const classes = useStyles();
   return (
     <img src={url} className={clsx(classes.image, className)} {...props} />
+  );
+};
+
+ImageContent.propTypes = {
+  slideshow: PropTypes.object.isRequired,
+  url: PropTypes.string.isRequired,
+  className: PropTypes.string
+};
+
+const TRANSITION_TIMEOUT = 500;
+
+const transitions = {
+  none: { component: React.Fragment },
+  collapse: { component: mui.Collapse, props: { in: true, timeout: TRANSITION_TIMEOUT } },
+  fade: { component: mui.Fade, props: { in: true, timeout: TRANSITION_TIMEOUT } },
+  grow: { component: mui.Grow, props: { in: true, timeout: TRANSITION_TIMEOUT } },
+  slide: { component: mui.Slide, props: { in: true, direction: 'right', timeout: TRANSITION_TIMEOUT } },
+  zoom: { component: mui.Zoom, props: { in: true, timeout: TRANSITION_TIMEOUT } },
+};
+
+const Image = ({ slideshow, url, className, ...props }) => {
+  const classes = useStyles();
+
+  const transitionData = transitions[slideshow.transition];
+  if(!transitionData) {
+    throw new Error(`Unknown transition: ${transitionData}`);
+  }
+
+  const { component: Transition, props: transitionProps } = transitionData;
+  return (
+    <Transition key={url} {...transitionProps}>
+      <img src={url} className={clsx(classes.image, className)} {...props} />
+    </Transition>
   );
 };
 
