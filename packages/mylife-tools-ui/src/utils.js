afdcc638c24fc766810b87d623f37438d0f1c546
vincent-tr <vincent@stoic.com>
2020-01-24 12:47:56 +0100

    add utils

--- packages/mylife-tools-ui/src/utils.js
+++ packages/mylife-tools-ui/src/utils.js
@@ -3,3 +3,27 @@
 export function fireAsync(target) {
   target().catch(err => console.error('Unhandled promise rejection', err)); // eslint-disable-line no-console
 }
+
+export class AbortError extends Error {
+}
+
+// throws AbortError if aborted
+export async function abortableDelay(delay, controller) {
+  return new Promise((resolve, reject) => {
+    const signal = controller.signal;
+    let timer;
+
+    const abortHandler = () => {
+      clearTimeout(timer);
+      signal.removeEventListener('abort', abortHandler);
+      reject(new AbortError());
+    };
+
+    signal.addEventListener('abort', abortHandler);
+
+    timer = setTimeout(() => {
+      signal.removeEventListener('abort', abortHandler);
+      resolve();
+    }, delay);
+  });
+}
