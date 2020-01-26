891d387bcaa41e86d67c2dc3a88b9dd87304bb35
vincent-tr <vincent.tr@gmail.com>
2020-01-26 18:35:08 +0100

    details wip

--- packages/mylife-gallery/public/src/album/components/index.js
+++ packages/mylife-gallery/public/src/album/components/index.js
@@ -2,7 +2,7 @@
 
 import { React, PropTypes, useMemo, mui, useSelector, useDispatch, useLifecycle } from 'mylife-tools-ui';
 import { enter, leave } from '../actions';
-import { getAlbum, getDocuments } from '../selectors';
+import { getAlbum, getDocuments, isShowDetail } from '../selectors';
 import DocumentThumbnailList from '../../common/document-thumbnail-list';
 import ListFooter from '../../common/list-footer';
 
@@ -11,7 +11,8 @@ const useConnect = () => {
   return {
     ...useSelector(state => ({
       album: getAlbum(state),
-      documents: getDocuments(state)
+      documents: getDocuments(state),
+      isShowDetail: isShowDetail(state),
     })),
     ...useMemo(() => ({
       enter: (albumId) => dispatch(enter(albumId)),
@@ -23,24 +24,38 @@ const useConnect = () => {
 const useStyles = mui.makeStyles({
   container: {
     display: 'flex',
+    flex: '1 1 auto',
+  },
+  listContainer: {
+    display: 'flex',
     flexDirection: 'column',
     flex: '1 1 auto',
-    overflowY: 'auto'
   },
   list: {
     flex: '1 1 auto'
+  },
+  detail: {
+    width: 350,
+    overflowY: 'auto'
   }
 });
 
 const Album = ({ albumId }) => {
   const classes = useStyles();
-  const { enter, leave, album, documents } = useConnect();
+  const { enter, leave, album, documents, isShowDetail } = useConnect();
   useLifecycle(() => enter(albumId), leave);
 
   return (
     <div className={classes.container}>
-      <DocumentThumbnailList className={classes.list} data={documents}  />
-      <ListFooter text={`${documents.length} document(s)`} />
+      <div className={classes.listContainer}>
+        <DocumentThumbnailList className={classes.list} data={documents}  />
+        <ListFooter text={`${documents.length} document(s)`} />
+      </div>
+      {isShowDetail &&(
+        <div className={classes.detail}>
+          Detail
+        </div>
+      )}
     </div>
   );
 };
