3c51ee098a870b0466a42f9fcec6895c9d63e19c
vincent-tr <vincent@stoic.com>
2020-01-30 10:57:09 +0100

    wip keywords layout

--- packages/mylife-gallery/public/src/document-list/components/header-keywords.js
+++ packages/mylife-gallery/public/src/document-list/components/header-keywords.js
@@ -1,12 +1,17 @@
 'use strict';
 
-import { React, PropTypes, mui } from 'mylife-tools-ui';
+import { React, PropTypes, mui, immutable, useMemo } from 'mylife-tools-ui';
+import { getFieldName } from '../../common/metadata-utils';
+import { useKeywordView } from '../../common/keyword-view';
 
 const useStyles = mui.makeStyles(theme => {
   const iconColor = mui.fade(theme.palette.text.primary, 0.26);
   return {
-    // like Chip impl
+    editor: {
+      marginLeft: theme.spacing(1)
+    },
     addIcon: {
+      // like Chip impl
       color: iconColor,
       '&:hover': {
         color: mui.fade(iconColor, 0.4)
@@ -17,44 +22,82 @@ const useStyles = mui.makeStyles(theme => {
   };
 });
 
+const Chip = ({ documents, keywordUsage, keyword, onAdd, ...props }) => {
+  const classes = useStyles();
+  const usage = keywordUsage.get(keyword);
+  const canAdd = usage.size < documents.length;
+  const handleAdd = e => {
+    e.stopPropagation();
+    onAdd();
+  };
+
+  return (
+    <mui.Chip variant='outlined' label={
+      <>
+        {keyword}
+        {canAdd && (
+          <mui.Tooltip title={'Ajouter le mot clé à toute la sélection'}>
+            <mui.IconButton
+              size='small'
+              className={classes.addIcon}
+              onClick={handleAdd}
+            >
+              <mui.icons.AddCircle />
+            </mui.IconButton>
+          </mui.Tooltip>
+        )}
+      </>
+    } {...props} />
+  );
+};
+
+Chip.propTypes = {
+  documents: PropTypes.array.isRequired,
+  keywordUsage: PropTypes.object.isRequired,
+  keyword: PropTypes.string.isRequired,
+  onAdd: PropTypes.func.isRequired
+};
+
 const HeaderKeywords = ({ documents }) => {
   const classes = useStyles();
-  const values = ['vmot1', 'vmot2', 'vmot3'];
-  const onChange = (values) => console.log(values);
+  const keywordUsage = useMemo(() => getKeywordUsage(documents), [documents]);
+  const list = useMemo(() => keywordUsage.keySeq().toArray(), [keywordUsage]);
+  const { keywords: options } = useKeywordView();
+
+  console.log(keywordUsage);
+  const onValuesChange = (values) => console.log(values);
+  const onAddAll = (keyword) => console.log(keyword);
+
   return (
     <>
       <mui.Typography>
-        {'Mots clés'}
+        {getFieldName('document', 'keywords')}
       </mui.Typography>
-      
+
       <mui.Autocomplete
+        className={classes.editor}
+        filterSelectedOptions
         multiple
-        options={['mot1', 'mot2', 'mot3']}
+        options={options}
         freeSolo
         disableClearable
         renderTags={(values, getTagProps) =>
-          values.map((value, index) => (
-            <mui.Chip key={index} variant='outlined' label={
-              <>
-                {value}
-                <mui.Tooltip title={'Ajouter le mot clé à toute la sélection'}>
-                  <mui.IconButton size='small' className={classes.addIcon}>
-                    <mui.icons.AddCircle />
-                  </mui.IconButton>
-                </mui.Tooltip>
-              </>
-            } {...getTagProps({ index })} />
+          values.map((keyword, index) => (
+            <Chip
+              key={index}
+              documents={documents}
+              keywordUsage={keywordUsage}
+              keyword={keyword}
+              onAdd={() => onAddAll(keyword)}
+              {...getTagProps({ index })}
+            />
           ))
         }
         renderInput={params => (
-          <mui.TextField
-            {...params}
-            placeholder='Mot clé'
-            fullWidth
-          />
+          <mui.TextField {...params} fullWidth />
         )}
-        value={values}
-        onChange={(e, values) => onChange(values)}
+        value={list}
+        onChange={(e, values) => onValuesChange(values)}
       />
     </>
   );
@@ -65,3 +108,21 @@ HeaderKeywords.propTypes = {
 };
 
 export default HeaderKeywords;
+
+function getKeywordUsage(documents) {
+  const keywords = new Map();
+  for(const { document } of documents) {
+    for(const keyword of document.keywords || []) {
+      let documents = keywords.get(keyword);
+      if(!documents) {
+        documents = new Set();
+        keywords.set(keyword, documents);
+      }
+
+      documents.add(document);
+    }
+  }
+  const entries = Array.from(keywords.entries());
+  const setEntries = entries.map(([keyword, documents]) => [keyword, new immutable.Set(documents)]);
+  return new immutable.Map(setEntries);
+}
