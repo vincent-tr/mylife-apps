aeb5ae15aed33b54c08baabfcf3d1209fcc05cd7
vincent-tr <vincent.tr@gmail.com>
2020-02-25 14:42:35 +0100

    wip strategy

--- packages/mylife-trading/ts-src/strategy-1.ts
+++ packages/mylife-trading/ts-src/strategy-1.ts
@@ -1,33 +1,64 @@
+import { RSI, BollingerBands } from 'technicalindicators';
 import Strategy from './strategy';
 import { Datasource, Resolution } from './datasource';
 import MovingDataset from './moving-dataset';
-
+import { last } from './utils'
 
 export default class Strategy1 implements Strategy {
   private datasource: Datasource;
   private dataset: MovingDataset;
+  private lastProcessedTimestamp: number;
 
   async init() {
     this.datasource = new Datasource({ key: process.env.IGKEY, identifier: process.env.IGID, password: process.env.IGPASS, isDemo: true });
     await this.datasource.init();
     console.log('datasource init');
 
-    this.dataset = await this.datasource.getDataset('CS.D.EURUSD.CFD.IP', Resolution.MINUTE, 15);
+    this.dataset = await this.datasource.getDataset('CS.D.EURUSD.CFD.IP', Resolution.MINUTE, 16);
     this.dataset.on('error', err => console.error('ERROR', err));
-    this.dataset.on('add', record => console.log(record));
+    this.dataset.on('add', () => this.onDatasetChange());
+    this.dataset.on('update', () => this.onDatasetChange());
 
-    this.dataset.on('update', record => console.log('UPDATE', record));
-
-    for (const record of this.dataset.list) {
-      console.log(record);
-    }
-
+    this.onDatasetChange();
   }
 
   async terminate() {
     this.dataset.close();
     await this.datasource.terminate();
     console.log('datasource terminate');
+  }
+
+  private onDatasetChange() {
+    if(!this.shouldRun()) {
+      return;
+    }
+
+    const period = 14;
+    const { fixedList } = this.dataset;
+    const values = fixedList.map(record => record.average.close);
+
+    const rsi = last(RSI.calculate({ period, values }));
+    const bb = last(BollingerBands.calculate({ period, values, stdDev: 2 }));
+    const candle = last(fixedList);
+
+    if(rsi > 70 && candle.ask.close > bb.upper) {
+      console.log(new Date().toISOString(), 'SHOULD SELL', rsi, candle.ask.close, bb.upper);
+    }
+
+    if(rsi < 30 && candle.ask.close < bb.lower) {
+      console.log(new Date().toISOString(), 'SHOULD BUY', rsi, candle.ask.close, bb.lower);
+    }
+  }
+
+  private shouldRun() {
+    const fixedRecords = this.dataset.fixedList;
+    const lastTimestamp = fixedRecords[fixedRecords.length - 1].timestamp.valueOf();
+    if(lastTimestamp === this.lastProcessedTimestamp) {
+      return false;
+    }
+
+    this.lastProcessedTimestamp = lastTimestamp
+    return true;
   }
 }
 
