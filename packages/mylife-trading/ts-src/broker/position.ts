27cec91cf621672b8716adf1a0e0c26b7b2d51fb
vincent-tr <vincent.tr@gmail.com>
2020-02-26 14:39:47 +0100

    skeleton

--- packages/mylife-trading/ts-src/broker/position.ts
+++ packages/mylife-trading/ts-src/broker/position.ts
@@ -1,5 +1,19 @@
-export default class Position {
-  constructor() {
+import EventEmitter from 'events';
 
+declare interface MovingDataset {
+  on(event: 'error', listener: (err: Error) => void): this;
+  on(event: 'close', listener: () => void): this;
+}
+
+export default class Position extends EventEmitter {
+  constructor() {
+    super();
+  }
+
+  async update() {
+    
+  }
+
+  async close() {
   }
 }
