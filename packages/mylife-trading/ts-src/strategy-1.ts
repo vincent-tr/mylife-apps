cb5460282575638537d093a54306e433829d711f
vincent-tr <vincent.tr@gmail.com>
2020-03-10 16:56:09 +0100

    strategy skeleton

--- packages/mylife-trading/ts-src/strategy-1.ts
+++ packages/mylife-trading/ts-src/strategy-1.ts
@@ -3,10 +3,16 @@ import Strategy from './strategy';
 import { Broker, Resolution, MovingDataset } from './broker';
 import { last } from './utils';
 
+enum Status {
+  LOOKING,
+  ACTING
+}
+
 export default class Strategy1 implements Strategy {
   private datasource: Broker;
   private dataset: MovingDataset;
   private lastProcessedTimestamp: number;
+  private status: Status;
 
   async init() {
     this.datasource = new Broker({ key: process.env.IGKEY, identifier: process.env.IGID, password: process.env.IGPASS, isDemo: true });
@@ -17,6 +23,8 @@ export default class Strategy1 implements Strategy {
     this.dataset.on('error', err => console.error('ERROR', err));
     this.dataset.on('add', () => this.onDatasetChange());
     this.dataset.on('update', () => this.onDatasetChange());
+
+    this.status = Status.LOOKING;
 
     this.onDatasetChange();
   }
@@ -32,6 +40,29 @@ export default class Strategy1 implements Strategy {
       return;
     }
 
+    switch (this.status) {
+      case Status.LOOKING:
+        this.lookForPosition();
+        break;
+
+      case Status.ACTING:
+        this.lookForRelease();
+        break;
+    }
+  }
+
+  private shouldRun() {
+    const fixedRecords = this.dataset.fixedList;
+    const lastTimestamp = fixedRecords[fixedRecords.length - 1].timestamp.valueOf();
+    if (lastTimestamp === this.lastProcessedTimestamp) {
+      return false;
+    }
+
+    this.lastProcessedTimestamp = lastTimestamp;
+    return true;
+  }
+
+  private lookForPosition() {
     const period = 14;
     const { fixedList } = this.dataset;
     const values = fixedList.map(record => record.average.close);
@@ -47,17 +78,11 @@ export default class Strategy1 implements Strategy {
     if (rsi < 30 && candle.average.close < bb.lower) {
       console.log(new Date().toISOString(), 'SHOULD BUY', rsi, candle.average.close, bb.lower);
     }
+
   }
 
-  private shouldRun() {
-    const fixedRecords = this.dataset.fixedList;
-    const lastTimestamp = fixedRecords[fixedRecords.length - 1].timestamp.valueOf();
-    if (lastTimestamp === this.lastProcessedTimestamp) {
-      return false;
-    }
-
-    this.lastProcessedTimestamp = lastTimestamp;
-    return true;
+  private lookForRelease() {
+    // TODO
   }
 }
 
