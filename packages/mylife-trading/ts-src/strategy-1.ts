e8de8f9835a9c036c7128c9195f3f1457b9fa392
vincent-tr <vincent.tr@gmail.com>
2020-03-21 18:36:14 +0100

    wip position

--- packages/mylife-trading/ts-src/strategy-1.ts
+++ packages/mylife-trading/ts-src/strategy-1.ts
@@ -1,21 +1,28 @@
 import { RSI, BollingerBands } from 'technicalindicators';
+import { createLogger } from 'mylife-tools-server';
 import Strategy from './strategy';
-import { Broker, Resolution, MovingDataset, DealDirection, Position } from './broker';
+import { Broker, Resolution, MovingDataset, DealDirection, Position, InstrumentDetails } from './broker';
 import { last } from './utils';
 import { BollingerBandsOutput } from 'technicalindicators/declarations/volatility/BollingerBands';
+
+const logger = createLogger('mylife:trading:strategy-1');
 
 export default class Strategy1 implements Strategy {
   private datasource: Broker;
   private dataset: MovingDataset;
   private lastProcessedTimestamp: number;
   private position: Position;
+  private instrument: InstrumentDetails;
 
   async init() {
     this.datasource = new Broker({ key: process.env.IGKEY, identifier: process.env.IGID, password: process.env.IGPASS, isDemo: true });
     await this.datasource.init();
-    console.log('datasource init');
+    logger.debug('datasource init');
 
-    this.dataset = await this.datasource.getDataset('CS.D.EURUSD.CFD.IP', Resolution.MINUTE, 16);
+    const market = await this.datasource.getEpic('CS.D.EURUSD.CFD.IP');
+    this.instrument = market.instrument;
+
+    this.dataset = await this.datasource.getDataset(this.instrument.epic, Resolution.MINUTE, 16);
     this.dataset.on('error', err => console.error('ERROR', err));
     this.dataset.on('add', () => this.onDatasetChange());
     this.dataset.on('update', () => this.onDatasetChange());
@@ -26,7 +33,7 @@ export default class Strategy1 implements Strategy {
   async terminate() {
     this.dataset.close();
     await this.datasource.terminate();
-    console.log('datasource terminate');
+    logger.debug('datasource terminate');
   }
 
   private onDatasetChange() {
@@ -62,8 +69,7 @@ export default class Strategy1 implements Strategy {
 
   private async takePosition(direction: DealDirection, bb: BollingerBandsOutput) {
     const size = 1; // TODO
-    const currencyCode = 'EUR'; // TODO: get from instrument
-    this.position = await this.datasource.openPosition('CS.D.EURUSD.CFD.IP', currencyCode, direction, size, { distance: 5 }, { level: bb.middle });
+    this.position = await this.datasource.openPosition(this.instrument, direction, size, { distance: 10 }, { level: bb.middle }); // stop loss 10 = min for ig
 
     this.position.on('close', () => {
       console.log('POSITION CLOSED');
@@ -82,6 +88,8 @@ export default class Strategy1 implements Strategy {
     }
 
     // see if we can take position
+    await this.takePosition(DealDirection.SELL, bb);
+    return;
 
     if (rsi > 70 && candle.average.close > bb.upper) {
       console.log(new Date().toISOString(), 'SHOULD SELL', rsi, candle.average.close, bb.upper);
