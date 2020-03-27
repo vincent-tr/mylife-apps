fd2b7ef6074fa1de920c1240c71a378f6dc639b4
vincent-tr <vincent.tr@gmail.com>
2020-03-27 14:29:40 +0100

    layout

--- packages/mylife-trading/public/src/brokers/components/broker.js
+++ packages/mylife-trading/public/src/brokers/components/broker.js
@@ -12,6 +12,12 @@ const useConnect = () => {
   }), [dispatch]);
 };
 
+const useStyles = mui.makeStyles(theme => ({
+  container: {
+    padding: theme.spacing(2),
+  },
+}));
+
 const PasswordField = ({ crypted, onSet, ...props }) => {
   const [value, setValue] = useState('');
   // reset value on change
@@ -47,50 +53,53 @@ const PasswordField = ({ crypted, onSet, ...props }) => {
 };
 
 const Broker = ({ broker }) => {
+  const classes = useStyles();
   const { update, remove } = useConnect();
 
   return (
-    <mui.Grid container spacing={2}>
-      <mui.Grid item xs={6}>
-        <mui.Typography>{getFieldName('broker', 'display')}</mui.Typography>
-      </mui.Grid>
-      <mui.Grid item xs={6}>
-        <DebouncedTextField value={broker.display} onChange={display => update(broker, { display })} fullWidth />
-      </mui.Grid>
-      <mui.Grid item xs={6}>
-        <mui.Typography>{getFieldName('broker', 'key')}</mui.Typography>
-      </mui.Grid>
-      <mui.Grid item xs={6}>
-        <DebouncedTextField value={broker.key} onChange={key => update(broker, { key })} fullWidth />
-      </mui.Grid>
-      <mui.Grid item xs={6}>
-        <mui.Typography>{getFieldName('broker', 'identifier')}</mui.Typography>
-      </mui.Grid>
-      <mui.Grid item xs={6}>
-        <DebouncedTextField value={broker.identifier} onChange={identifier => update(broker, { identifier })} fullWidth />
-      </mui.Grid>
-      <mui.Grid item xs={6}>
-        <mui.Typography>{getFieldName('broker', 'password')}</mui.Typography>
-      </mui.Grid>
-      <mui.Grid item xs={6}>
-        <PasswordField crypted={broker.password} onSet={password => update(broker, { password })} fullWidth />
-      </mui.Grid>
-      <mui.Grid item xs={6}>
-        <mui.Typography>{getFieldName('broker', 'demo')}</mui.Typography>
-      </mui.Grid>
-      <mui.Grid item xs={6}>
-        <mui.Checkbox checked={broker.demo} onChange={e => update(broker, { demo: e.target.checked })} />
-      </mui.Grid>
+    <mui.Paper variant='outlined' square className={classes.container}>
+      <mui.Grid container spacing={2}>
+        <mui.Grid item xs={6}>
+          <mui.Typography>{getFieldName('broker', 'display')}</mui.Typography>
+        </mui.Grid>
+        <mui.Grid item xs={6}>
+          <DebouncedTextField value={broker.display} onChange={display => update(broker, { display })} fullWidth />
+        </mui.Grid>
+        <mui.Grid item xs={6}>
+          <mui.Typography>{getFieldName('broker', 'key')}</mui.Typography>
+        </mui.Grid>
+        <mui.Grid item xs={6}>
+          <DebouncedTextField value={broker.key} onChange={key => update(broker, { key })} fullWidth />
+        </mui.Grid>
+        <mui.Grid item xs={6}>
+          <mui.Typography>{getFieldName('broker', 'identifier')}</mui.Typography>
+        </mui.Grid>
+        <mui.Grid item xs={6}>
+          <DebouncedTextField value={broker.identifier} onChange={identifier => update(broker, { identifier })} fullWidth />
+        </mui.Grid>
+        <mui.Grid item xs={6}>
+          <mui.Typography>{getFieldName('broker', 'password')}</mui.Typography>
+        </mui.Grid>
+        <mui.Grid item xs={6}>
+          <PasswordField crypted={broker.password} onSet={password => update(broker, { password })} fullWidth />
+        </mui.Grid>
+        <mui.Grid item xs={6}>
+          <mui.Typography>{getFieldName('broker', 'demo')}</mui.Typography>
+        </mui.Grid>
+        <mui.Grid item xs={6}>
+          <mui.Checkbox checked={broker.demo} onChange={e => update(broker, { demo: e.target.checked })} />
+        </mui.Grid>
 
-      <DeleteButton
-        tooltip={'Supprimer le compte'}
-        icon
-        text='Supprimer'
-        confirmText={`Etes-vous sûr de vouloir supprimer le compte '${renderObject(broker)}' ?`}
-        onConfirmed={() => remove(broker)}
-      />
+        <DeleteButton
+          tooltip={'Supprimer le compte'}
+          icon
+          text='Supprimer'
+          confirmText={`Etes-vous sûr de vouloir supprimer le compte '${renderObject(broker)}' ?`}
+          onConfirmed={() => remove(broker)}
+        />
 
-    </mui.Grid>
+      </mui.Grid>
+    </mui.Paper>
   );
 };
 
