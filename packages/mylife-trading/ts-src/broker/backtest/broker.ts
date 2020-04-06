414de97f6c83d43b96e869f387585ad543a2003c
vincent-tr <vincent.tr@gmail.com>
2020-04-06 14:56:51 +0200

    fireAsync implementation

--- packages/mylife-trading/ts-src/broker/backtest/broker.ts
+++ packages/mylife-trading/ts-src/broker/backtest/broker.ts
@@ -1,3 +1,5 @@
+import { createLogger } from 'mylife-tools-server';
+
 import { Resolution, Credentials, Broker, PositionSummary, OpenPositionBound } from '../broker';
 import MovingDataset, { Record, CandleStickData } from '../moving-dataset';
 import Position, { PositionDirection } from '../position';
@@ -7,8 +9,11 @@ import { Timeline } from './timeline';
 import BacktestMarket from './market';
 import BacktestInstrument from './instrument';
 
+const logger = createLogger('mylife:trading:broker:backtest');
+
 export class BacktestBroker implements Broker {
   private timeline: Timeline;
+  private readonly pendingPromises = new Set<Promise<void>>();
 
   getMarket(instrumentId: string): Market {
     const { market } = parseInstrumentId(instrumentId);
@@ -16,7 +21,18 @@ export class BacktestBroker implements Broker {
   }
 
   fireAsync(target: () => Promise<void>): void {
-    throw new Error('Method not implemented.');
+    const deferred = createDeferred<void>();
+    this.pendingPromises.add(deferred.promise);
+
+    target().catch(err => logger.error(`Unhandled promise rejection: ${err.stack}`)).finally(() => {
+      this.pendingPromises.delete(deferred.promise);
+      deferred.resolve();
+    });
+  }
+
+  private async waitAllAsync() {
+    const pendings = Array.from(this.pendingPromises);
+    return Promise.all(pendings);
   }
 
   async init(credentials: Credentials) {
@@ -50,4 +66,22 @@ function parseInstrumentId(instrumentId: string) {
     throw new Error(`Malformed instrument id: '${instrumentId}'`);
   }
   return { market, instrument };
+}
+
+interface Deferred<T> {
+  readonly promise: Promise<T>;
+  readonly resolve: (value: T) => void;
+  readonly reject: (err: Error) => void;
+};
+
+function createDeferred<T>() : Deferred<T> {
+  let resolve: (value: T) => void;
+  let reject: (err: Error) => void;
+
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res;
+    reject = rej;
+  });
+
+  return { promise, resolve, reject };
 }
