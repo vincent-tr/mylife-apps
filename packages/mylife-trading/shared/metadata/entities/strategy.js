7de04ff2e898297307767f09aac5c5f13674e890
vincent-tr <vincent.tr@gmail.com>
2020-04-09 10:23:09 +0200

    meta fix

--- packages/mylife-trading/shared/metadata/entities/strategy.js
+++ packages/mylife-trading/shared/metadata/entities/strategy.js
@@ -10,7 +10,7 @@ module.exports = {
     { id: 'enabled', name: 'Activé', datatype: 'boolean', constraints: ['not-null'] },
     { id: 'broker', name: 'Compte de courtage', datatype: 'broker', constraints: ['not-null'] },
     { id: 'instrumentId', name: 'Instrument de trading', datatype: 'name', constraints: ['not-null', 'not-empty'] },
-    { id: 'risk', name: 'Valeur de risque', description: 'Valeur qu\'on est pret a perdre, dans la devise du compte, pour une prise de position', datatype: 'name', constraints: ['not-null', 'positive'] },
+    { id: 'risk', name: 'Valeur de risque', description: 'Valeur qu\'on est pret a perdre, dans la devise du compte, pour une prise de position', datatype: 'amount', constraints: ['not-null', 'positive'] },
   ],
   display: obj => obj.display
 };
