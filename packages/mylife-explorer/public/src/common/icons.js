893b54e4c6932172774abd6ebb1573bb3bc01f43
vincent-tr <vincent.tr@gmail.com>
2020-04-24 21:05:02 +0200

    fix icon

--- packages/mylife-explorer/public/src/common/icons.js
+++ packages/mylife-explorer/public/src/common/icons.js
@@ -23,7 +23,7 @@ const {
 */
 const Explorer = (props) => (
   <mui.SvgIcon viewBox="0 0 24 24" {...props}>
-    <path fill="#4da6ff" d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z"/>
+    <path d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z"/>
   </mui.SvgIcon>  
 );
 
