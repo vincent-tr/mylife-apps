3de05d04970575fa46e1ea34db8ab698b6696f64
vincent-tr <vincent.tr@gmail.com>
2020-04-30 13:43:19 +0200

    give up with video fancy stuff

--- packages/mylife-explorer/public/src/home/components/viewer/video.js
+++ packages/mylife-explorer/public/src/home/components/viewer/video.js
@@ -1,34 +1,16 @@
 'use strict';
 
-import { React, PropTypes, clsx, mui } from 'mylife-tools-ui';
+import { React, PropTypes, clsx } from 'mylife-tools-ui';
 import { makeUrl } from '../tools';
 
-// https://github.com/Afterster/videojs-vlc/blob/HEAD/example.html
-// https://github.com/videojs/video.js/issues/2750#issuecomment-151939927
-
-// import 'video.js/dist/video-js.css';
-// import 'script-loader!video.js/dist/video';
-// import 'script-loader!videojs-vlc/lib/videojs-vlc';
-
-const useStyles = mui.makeStyles({
-  video: {
-    width: '100%',
-    height: '100%'
-  }
-});
-
-const Video = ({ data, className, ...props }) => {
-  const classes = useStyles();
-  return (
-    <video 
-      controls
-      className={clsx(className)}
-      {...props}
-    >
-      <source src={makeUrl(data)} />
-    </video>
-  );
-};
+const Video = ({ data, className, ...props }) => (
+  <video 
+    controls
+    src={makeUrl(data)} 
+    className={clsx(className)}
+    {...props}
+  />
+);
 
 Video.propTypes = {
   data: PropTypes.object.isRequired,
