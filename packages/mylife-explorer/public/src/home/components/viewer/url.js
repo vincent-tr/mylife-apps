9b7c0fb75d818d05f05b683660fa6cce475052c2
vincent-tr <vincent.tr@gmail.com>
2020-04-30 15:40:16 +0200

    add .url support

--- packages/mylife-explorer/public/src/home/components/viewer/url.js
+++ packages/mylife-explorer/public/src/home/components/viewer/url.js
@@ -1,16 +1,63 @@
 'use strict';
 
-import { React, PropTypes, clsx } from 'mylife-tools-ui';
+import { React, PropTypes, mui, fireAsync, useState, useEffect } from 'mylife-tools-ui';
 import { makeUrl } from '../tools';
+import ini from 'ini';
 
-const Url = ({ data, ...props }) => (
-  <div {...props}>
-    {makeUrl(data)}
-  </div>
-);
+const useStyles = mui.makeStyles(theme => ({
+  content: {
+    margin: theme.spacing(2)
+  }
+}));
+
+const Url = ({ data, ...props }) => {
+  const url = useUrl(data);
+  const classes = useStyles();
+
+  if(!url) {
+    return (
+      <div {...props}>
+        <mui.Typography className={classes.content}>
+          {`Chargement ...`}
+        </mui.Typography>
+      </div>
+    );
+  }
+
+  return (
+    <div {...props}>
+      <mui.Tooltip title={url}>
+        <mui.Button variant='contained' color='primary' href={url} target='_blank' className={classes.content}>
+          Ouvrir
+        </mui.Button>
+      </mui.Tooltip>
+    </div>
+  );
+};
 
 Url.propTypes = {
   data: PropTypes.object.isRequired,
 };
 
 export default Url;
+
+function useUrl(data) {
+  const sourceUrl = makeUrl(data);
+  const [result, setResult] = useState(null);
+  useEffect(() => {
+    fireAsync(async () => {
+      setResult(null);
+      const res = await fetchAndParse(sourceUrl);
+      setResult(res);
+    });
+  }, [sourceUrl]);
+
+  return result;
+}
+
+async function fetchAndParse(url) {
+  const response = await fetch(url);
+  const text = await response.text();
+  const content = ini.parse(text);
+  return content.InternetShortcut.URL;
+}
