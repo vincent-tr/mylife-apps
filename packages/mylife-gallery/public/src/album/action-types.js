e27e44e89600bb5371d1f0e6e11bd1fac9244e8e
vincent-tr <vincent.tr@gmail.com>
2020-05-26 11:30:05 +0200

    cleanup

--- packages/mylife-gallery/public/src/album/action-types.js
+++ packages/mylife-gallery/public/src/album/action-types.js
@@ -4,7 +4,5 @@ import { constants } from 'mylife-tools-ui';
 
 export default constants.wrap({
   SET_ALBUM_ID : null,
-  SET_ALBUM_VIEW : null,
-  SET_DOCUMENT_VIEW : null,
   SHOW_DETAIL : null,
 }, 'album');
