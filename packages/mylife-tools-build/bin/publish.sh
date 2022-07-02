e8a8df7b5a3e929d07acba3fc0c9bda06205de34
vincent-tr <vincent.tr@gmail.com>
2022-07-02 15:40:38 +0200

    fix script

--- packages/mylife-tools-build/bin/publish.sh
+++ packages/mylife-tools-build/bin/publish.sh
@@ -2,5 +2,5 @@
 
 package_name=$(node -p "require(path.join(process.cwd(), './package.json')).name")
 package_version=$(node -p "require(path.join(process.cwd(), './package.json')).version")
-docker build --pull -t "vincenttr/$package_name:$package_version" --build-arg "package_name=$package_name" --build-arg "package_version=$package_version" - < "$(dirname $(realpath ${BASH_SOURCE}))/../docker/Dockerfile"
+docker build --pull -t "vincenttr/$package_name:$package_version" -f "$(dirname $(realpath ${BASH_SOURCE}))/../docker/Dockerfile" .
 docker push "vincenttr/$package_name:$package_version"
