6343e82864584cc376a649769c3b7d5b188f772d
vincent-tr <vincent.tr@gmail.com>
2022-07-04 14:31:17 +0200

    use ffmpeg for image conversion as well

--- packages/mylife-gallery/src/sync/loaders/image.js
+++ packages/mylife-gallery/src/sync/loaders/image.js
@@ -1,10 +1,15 @@
 'use strict';
 
+const path = require('path');
+const os = require('os');
+const fs = require('fs-extra');
+const { PassThrough } = require('stream');
+
 const { createLogger } = require('mylife-tools-server');
 const business = require('../../business');
 const { Image } = require('./tools/jimp');
 const { getMetadata } = require('./tools/exif');
-const { convertBufferToWebpBuffer, convertBufferToWebpStream } = require('./tools/webp');
+const { toWebPStream } = require('./tools/ffmpeg');
 
 const logger = createLogger('mylife:gallery:sync:loaders:image');
 
@@ -27,16 +32,68 @@ exports.processImage = async (content, relativePath) => {
   // do not try/catch if we could not read the image
   const image = await Image.load(content);
 
-  const thumbnailContent = await convertBufferToWebpBuffer(await image.thumbnailPngBuffer());
-  const thumbnail = await business.thumbnailCreate(thumbnailContent);
+  const fsh = new FsHelper();
+  await fsh.init();
+  try {
+    const thBuffFile = await fsh.toFile(await image.thumbnailPngBuffer());
 
-  // from image to keep proper rotation
-  const imageStream = await convertBufferToWebpStream(await image.pngBuffer());
-  const media = await business.mediaCreate(imageStream, 'image/webp');
+    const thStream = new PassThrough();
+    await toWebPStream(thBuffFile, thStream);
 
-  const metadata = image.getMetadata();
-  Object.assign(values, { thumbnail, media, ...metadata });
+    const chunks = [];
+    for await (let chunk of thStream) {
+      chunks.push(chunk);
+    }
+    const thumbnailContent = Buffer.concat(chunks);
+    //---
 
-  logger.debug(`Image loaded '${relativePath}'`);
-  return values;
+    const thumbnail = await business.thumbnailCreate(thumbnailContent);
+
+    // from image to keep proper rotation
+    const buffFile = await fsh.toFile(await image.pngBuffer());
+    const imageStream = new PassThrough();
+    await toWebPStream(buffFile, imageStream);
+    //---
+    const media = await business.mediaCreate(imageStream, 'image/webp');
+
+    const metadata = image.getMetadata();
+    Object.assign(values, { thumbnail, media, ...metadata });
+
+    logger.debug(`Image loaded '${relativePath}'`);
+    return values;
+
+  } finally {
+    await fsh.terminate();
+  }
 };
+
+// TODO: share
+class FsHelper {
+  async init() {
+    this.counter = 0;
+    this.baseDirectory = await fs.mkdtemp(path.join(os.tmpdir(), 'ffmpeg-'));
+  }
+
+  async terminate() {
+    await fs.remove(this.baseDirectory);
+  }
+
+  async toFile(content) {
+    const fullPath = path.join(this.baseDirectory, `content-${++this.counter}`);
+    await fs.writeFile(fullPath, content);
+    return fullPath;
+  }
+
+  async list() {
+    const names = await fs.readdir(this.baseDirectory);
+    return names.map(name => path.join(this.baseDirectory, name));
+  }
+
+  async fromFile(fullPath) {
+    return fs.readFile(fullPath);
+  }
+
+  async fromFileRelative(relativePath) {
+    return fs.readFile(path.join(this.baseDirectory, relativePath));
+  }
+}
