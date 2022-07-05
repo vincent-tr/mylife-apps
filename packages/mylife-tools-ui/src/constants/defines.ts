d7ac48246a054f296effde63416172b1eb2c6f3f
vincent-tr <vincent.tr@gmail.com>
2022-07-05 14:25:35 +0200

    move stuff to ts

--- packages/mylife-tools-ui/src/constants/defines.ts
+++ packages/mylife-tools-ui/src/constants/defines.ts
@@ -1,3 +1,1 @@
-'use strict';
-
 export const STATE_PREFIX = 'mylife-tools-ui';
--- packages/mylife-tools-ui/src/constants/wrapper.ts
+++ packages/mylife-tools-ui/src/constants/wrapper.ts
@@ -1,21 +1,22 @@
-'use strict';
-
 const proxyHandler = {
   get: (target, name) => {
-    if(name in target) {
+    if (name in target) {
       return target[name];
     }
+
     throw new Error(`Invalid constant name: '${name}'`);
   }
 };
 
-export default function(target, ...prefixes) {
+export default function (target: { [key: string]: string; }, ...prefixes: string[]): { [key: string]: string; } {
   const prefix = prefixes.length ? prefixes.join(':') + ':' : '';
   const obj = {};
-  for(const [ key, value ] of Object.entries(target)) {
+
+  for (const [key, value] of Object.entries(target)) {
     const symbol = Symbol(prefix + (value || key));
     obj[key] = symbol;
   }
+
   Object.freeze(obj);
   return new Proxy(obj, proxyHandler);
 }
