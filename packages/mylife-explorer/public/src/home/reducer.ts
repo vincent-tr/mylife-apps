c77d87ea143e23af1421af8415cfb1c46ffd9794
vincent-tr <vincent@stoic.com>
2022-07-13 19:21:07 +0200

    wip move to ts

--- packages/mylife-explorer/public/src/home/reducer.ts
+++ packages/mylife-explorer/public/src/home/reducer.ts
@@ -1,16 +1,16 @@
-'use strict';
-
 import { handleActions } from 'mylife-tools-ui';
 import actionTypes from './action-types';
 
+type FIXME_any = any;
+
 export default handleActions({
 
-  [actionTypes.SET_DATA] : (state, action) => ({
+  [actionTypes.SET_DATA] : (state, action: FIXME_any) => ({
     ...state,
     data: action.payload
   }),
 
-  [actionTypes.SET_DETAIL] : (state, action) => ({
+  [actionTypes.SET_DETAIL] : (state, action: FIXME_any) => ({
     ...state,
     detail: action.payload
   }),
