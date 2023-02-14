83f3f89e4a06c8830656f252641dad634dca1c9c
vincent-tr <vincent.tr@gmail.com>
2023-02-14 11:26:27 +0100

    adapt energy

--- packages/mylife-energy/build/webpack.config.ts
+++
@@ -1,6 +0,0 @@
-import { prepare, createServerConfig } from 'mylife-tools-build/webpack.config';
-
-export default (env: Record<string, any>, argv: Record<string, any>) => {
-  const { baseDirectory, dev } = prepare(env, argv);
-  return createServerConfig(baseDirectory, dev);
-}
