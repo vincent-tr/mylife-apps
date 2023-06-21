6c8c2f2f60957bb805c1c931c38704086cf59000
vincent-tr <vincent.tr@gmail.com>
2023-06-21 17:02:24 +0200

    fix sensor def

--- packages/mylife-energy/shared/metadata/entities/sensor.ts
+++ packages/mylife-energy/shared/metadata/entities/sensor.ts
@@ -3,7 +3,7 @@ export default {
   parent: 'base',
   name: 'Capteur',
   fields: [
-    { id: 'sensorId', name: 'Date/heure', datatype: 'datetime', constraints: ['not-null'] },
+    { id: 'sensorId', name: 'Identifiant', datatype: 'name', constraints: ['not-null'] },
     { id: 'deviceClass', name: 'Classe du périphérique', datatype: 'name', constraints: ['not-null'] },
     { id: 'stateClass', name: 'Classe d\'état', datatype: 'name', constraints: ['not-null'] },
     { id: 'unitOfMeasurement', name: 'Unité de mesure', datatype: 'name', constraints: ['not-null'] },
