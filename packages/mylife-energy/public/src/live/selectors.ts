7fae2cc95d5a17d5b5cce190b4c7326f008b6f11
vincent-tr <vincent.tr@gmail.com>
2023-07-07 15:14:32 +0200

    cleanup

--- packages/mylife-energy/public/src/live/selectors.ts
+++ packages/mylife-energy/public/src/live/selectors.ts
@@ -2,7 +2,7 @@ import { views, createSelector } from 'mylife-tools-ui';
 import * as viewUids from './view-uids';
 import { DeviceType, LiveDevice, Measure } from '../../../shared/metadata';
 
-const getLive = state => state.live;
+// const getLive = state => state.live;
 export const getDeviceView = state => views.getView(state, viewUids.DEVICES) as views.View<LiveDevice>;
 export const getMeasureView = state => views.getView(state, viewUids.MEASURES) as views.View<Measure>;
 
--- packages/mylife-energy/public/src/live/view-uids.ts
+++ packages/mylife-energy/public/src/live/view-uids.ts
@@ -1,4 +1,2 @@
-'use strict';
-
 export const DEVICES = 'live-devices';
 export const MEASURES = 'live-measures';
