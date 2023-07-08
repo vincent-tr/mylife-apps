3bd36c36f96fe77a1e3b3473c5b82812487cd3ae
vincent-tr <vincent.tr@gmail.com>
2023-07-08 11:40:00 +0200

    filter builder

--- go/mylife-energy/pkg/services/query/filter_builder.go
+++ go/mylife-energy/pkg/services/query/filter_builder.go
@@ -6,9 +6,15 @@ type FilterExpression struct {
 	value bson.D
 }
 
-func makeFilterExpression(operator string, argument any) FilterExpression {
+func makeUnaryFilterExpression(operator string, argument any) FilterExpression {
 	return FilterExpression{
 		value: bson.D{{Key: operator, Value: argument}},
+	}
+}
+
+func makeBinaryFilterExpression(field string, operator string, value any) FilterExpression {
+	return FilterExpression{
+		value: bson.D{{Key: field, Value: bson.D{{Key: operator, Value: value}}}},
 	}
 }
 
@@ -17,23 +23,33 @@ func (expression FilterExpression) Build() interface{} {
 }
 
 func Eq(field string, value any) FilterExpression {
-	return makeFilterExpression("$eq", bson.D{{Key: field, Value: value}})
+	return makeBinaryFilterExpression(field, "$eq", value)
+}
+
+func In(field string, values ...any) FilterExpression {
+	argument := bson.A{}
+
+	for _, value := range values {
+		argument = append(argument, value)
+	}
+
+	return makeBinaryFilterExpression(field, "$in", argument)
 }
 
 func Gt(field string, value any) FilterExpression {
-	return makeFilterExpression("$gt", bson.D{{Key: field, Value: value}})
+	return makeBinaryFilterExpression(field, "$gt", value)
 }
 
 func Gte(field string, value any) FilterExpression {
-	return makeFilterExpression("$gte", bson.D{{Key: field, Value: value}})
+	return makeBinaryFilterExpression(field, "$gte", value)
 }
 
 func Lt(field string, value any) FilterExpression {
-	return makeFilterExpression("$lt", bson.D{{Key: field, Value: value}})
+	return makeBinaryFilterExpression(field, "$lt", value)
 }
 
 func Lte(field string, value any) FilterExpression {
-	return makeFilterExpression("$lte", bson.D{{Key: field, Value: value}})
+	return makeBinaryFilterExpression(field, "$lte", value)
 }
 
 func And(items ...FilterExpression) FilterExpression {
@@ -43,7 +59,7 @@ func And(items ...FilterExpression) FilterExpression {
 		argument = append(argument, item.value)
 	}
 
-	return makeFilterExpression("$and", argument)
+	return makeUnaryFilterExpression("$and", argument)
 }
 
 func Or(items ...FilterExpression) FilterExpression {
@@ -53,5 +69,5 @@ func Or(items ...FilterExpression) FilterExpression {
 		argument = append(argument, item.value)
 	}
 
-	return makeFilterExpression("$or", argument)
+	return makeUnaryFilterExpression("$or", argument)
 }
