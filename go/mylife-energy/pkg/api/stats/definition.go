c6b427c27ef2433d98038c5bb82e00da915fa981
vincent-tr <vincent.tr@gmail.com>
2023-07-19 17:20:46 +0200

    add devices view

--- go/mylife-energy/pkg/api/stats/definition.go
+++ go/mylife-energy/pkg/api/stats/definition.go
@@ -2,12 +2,15 @@ package stats
 
 import (
 	"mylife-energy/pkg/business/stats"
+	"mylife-energy/pkg/entities"
 	"mylife-tools-server/services/api"
+	"mylife-tools-server/services/notification"
 	"mylife-tools-server/services/sessions"
+	"mylife-tools-server/services/store"
 	"time"
 )
 
-var Definition = api.MakeDefinition("stats", getValues)
+var Definition = api.MakeDefinition("stats", getValues, notifyDevices)
 
 type statsInput struct {
 	Type      stats.StatsType
@@ -19,4 +22,18 @@ type statsOutput = stats.StatValues
 
 func getValues(session *sessions.Session, arg statsInput) ([]statsOutput, error) {
 	return stats.GetValues(arg.Type, arg.Timestamp, arg.Sensors)
+}
+
+func notifyDevices(session *sessions.Session, arg struct{}) (uint64, error) {
+	devices, err := store.GetCollection[*entities.Device]("devices")
+	if err != nil {
+		return 0, err
+	}
+
+	statableDevices := store.NewView[*entities.Device](devices, func(obj *entities.Device) bool {
+		return !obj.Computed() && obj.Type() == entities.Node
+	})
+
+	viewId := notification.NotifyView[*entities.Device](session, statableDevices)
+	return viewId, nil
 }
