34357c2365a517011f628c107446e5918ea384eb
vincent-tr <vincent.tr@gmail.com>
2024-09-18 17:57:37 +0200

    push latest

--- packages/mylife-tools-build/bin/publish.sh
+++ packages/mylife-tools-build/bin/publish.sh
@@ -2,5 +2,6 @@
 
 package_name=$(node -p "require(path.join(process.cwd(), './package.json')).name")
 package_version=$(node -p "require(path.join(process.cwd(), './package.json')).version")
-docker build --pull -t "vincenttr/$package_name:$package_version" -f "$(dirname $(realpath ${BASH_SOURCE}))/../docker/Dockerfile" .
+docker build --pull -t "vincenttr/$package_name:$package_version" -t "vincenttr/$package_name:latest" -f "$(dirname $(realpath ${BASH_SOURCE}))/../docker/Dockerfile" .
 docker push "vincenttr/$package_name:$package_version"
+docker push "vincenttr/$package_name:latest"
