f97e473b04132431db751e2aaf76353a4b547061
vincent-tr <vincent.tr@gmail.com>
2025-04-28 16:22:02 +0200

    operation stats

--- go/mylife-money/cmd/web.go
+++ go/mylife-money/cmd/web.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	ui "mylife-money-ui"
 	"mylife-money/pkg/api"
+	"mylife-money/pkg/business/views"
 	"mylife-money/pkg/entities"
 	"mylife-tools-server/services"
 	_ "mylife-tools-server/services/api"
@@ -18,7 +19,7 @@ func init() {
 		Run: func(_ *cobra.Command, _ []string) {
 			args := map[string]interface{}{
 				"api":   api.Definitions,
-				"store": entities.StoreDef,
+				"store": append(entities.CollectionsDef, views.ViewsDef...),
 				"web":   ui.FS,
 			}
 
