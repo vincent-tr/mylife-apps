3a519697bc724592dc0d341b585d849e13267fb5
vincent-tr <vincent.tr@gmail.com>
2025-04-29 10:24:22 +0200

    move StoreDef to business

--- go/mylife-money/cmd/web.go
+++ go/mylife-money/cmd/web.go
@@ -3,8 +3,7 @@ package cmd
 import (
 	ui "mylife-money-ui"
 	"mylife-money/pkg/api"
-	"mylife-money/pkg/business/views"
-	"mylife-money/pkg/entities"
+	"mylife-money/pkg/business"
 	"mylife-tools-server/services"
 	_ "mylife-tools-server/services/api"
 	_ "mylife-tools-server/services/web"
@@ -19,7 +18,7 @@ func init() {
 		Run: func(_ *cobra.Command, _ []string) {
 			args := map[string]interface{}{
 				"api":   api.Definitions,
-				"store": append(entities.CollectionsDef, views.ViewsDef...),
+				"store": business.StoreDef,
 				"web":   ui.FS,
 			}
 
