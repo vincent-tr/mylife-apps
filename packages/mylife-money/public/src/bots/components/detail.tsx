c3553a646916b4ff25b840746178874f46acf11a
vincent-tr <vincent.tr@gmail.com>
2025-05-19 12:10:15 +0200

    gui start bot

--- packages/mylife-money/public/src/bots/components/detail.tsx
+++ packages/mylife-money/public/src/bots/components/detail.tsx
@@ -1,7 +1,9 @@
-import { React, mui, clsx, CriteriaField } from 'mylife-tools-ui';
+import { React, mui, clsx, CriteriaField, useCallback, useAction, fireAsync } from 'mylife-tools-ui';
+import icons from '../../common/icons';
 import cronstrue from 'cronstrue';
 import 'cronstrue/locales/fr';
 import cronParser from 'cron-parser';
+import { startBot } from '../actions';
 
 type FIXME_any = any;
 type Bot = FIXME_any;
@@ -14,12 +16,20 @@ const useStyles = mui.makeStyles(theme => ({
 }));
 const Detail: React.FunctionComponent<{ bot: Bot; className?: string; }> = ({ bot, className }) => {
   const classes = useStyles();
+  const start = useStart(bot._id);
 
   return (
     <div className={clsx(classes.container, className)}>
       <mui.Grid container spacing={2}>
         <mui.Grid item xs={12}>
           <CronCriteriaField value={bot.schedule} />
+        </mui.Grid>
+        <mui.Grid item xs={12}>
+            <CriteriaField label={'Lancer le robot'}>
+              <mui.IconButton onClick={start}>
+                <icons.actions.Execute />
+              </mui.IconButton>
+            </CriteriaField>
         </mui.Grid>
       </mui.Grid>
     </div>
@@ -68,4 +78,9 @@ export function format(value: string) {
   }
 
   return cronstrue.toString(value, { locale: 'fr' });
+}
+
+function useStart(id: string) {
+  const start = useAction(startBot);
+  return useCallback(() => fireAsync(async () => start(id)), [id, start, fireAsync]);
 }
