138b65b6f4fc3e6debef77e89cfe23904564552f
vincent-tr <vincent.tr@gmail.com>
2025-05-19 11:19:01 +0200

    Update metadata

--- packages/mylife-money/shared/metadata/datatypes.ts
+++ packages/mylife-money/shared/metadata/datatypes.ts
@@ -1,6 +1,14 @@
 export default [
   { id: 'cron', primitive: 'string' },
   { id: 'bot-type', enum: ['noop', 'cic-scraper', 'frais-scraper', 'amazon-scraper', 'paypal-scraper'] },
+  { 
+    id: 'bot-run', structure: [
+      { id: 'start', name: 'Début', datatype: 'datetime' },
+      { id: 'end', name: 'Fin', datatype: 'datetime' },
+      { id: 'result', name: 'Résultat', datatype: 'bot-run-result' },
+      { id: 'logs', name: 'Logs', datatype: 'list:bot-run-log' }
+    ]
+  },
   { id: 'bot-run-result', enum: ['success', 'warning', 'error'] },
   {
     id: 'bot-run-log', structure: [
