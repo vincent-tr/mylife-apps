9ffe1ff4d6ba5fa04f063e9c6a708470800fa7a9
vincent-tr <vincent.tr@gmail.com>
2025-05-20 15:05:51 +0200

    wip bots engine

---
+++ go/mylife-money/pkg/services/bots/common/logger.go
@@ -0,0 +1,96 @@
+package common
+
+import (
+	"fmt"
+	"mylife-money/pkg/entities"
+	"mylife-tools-server/log"
+)
+
+var logger = log.CreateLogger("mylife:money:bots")
+
+type ExecutionLogger struct {
+	typ         entities.BotType
+	disp        *NotificationsDispatcher
+	maxSeverity entities.BotRunLogSeverity
+}
+
+func NewExecutionLogger(typ entities.BotType, disp *NotificationsDispatcher) *ExecutionLogger {
+	return &ExecutionLogger{
+		typ:         typ,
+		disp:        disp,
+		maxSeverity: entities.BotRunLogSeverityDebug,
+	}
+}
+
+func (el *ExecutionLogger) Log(message string, severity entities.BotRunLogSeverity) {
+	switch severity {
+	case entities.BotRunLogSeverityDebug:
+		logger.WithField("bot", el.typ).Debug(message)
+	case entities.BotRunLogSeverityInfo:
+		logger.WithField("bot", el.typ).Info(message)
+	case entities.BotRunLogSeverityWarning:
+		logger.WithField("bot", el.typ).Warning(message)
+	case entities.BotRunLogSeverityError:
+		logger.WithField("bot", el.typ).Error(message)
+	case entities.BotRunLogSeverityFatal:
+		logger.WithField("bot", el.typ).Fatal(message)
+	}
+
+	el.disp.EmitLog(el.typ, severity, message)
+
+	if severityOrder[severity] > severityOrder[el.maxSeverity] {
+		el.maxSeverity = severity
+	}
+}
+
+func (el *ExecutionLogger) GetMaxSeverity() entities.BotRunLogSeverity {
+	return el.maxSeverity
+}
+
+var severityOrder = map[entities.BotRunLogSeverity]int{
+	entities.BotRunLogSeverityDebug:   0,
+	entities.BotRunLogSeverityInfo:    1,
+	entities.BotRunLogSeverityWarning: 2,
+	entities.BotRunLogSeverityError:   3,
+	entities.BotRunLogSeverityFatal:   4,
+}
+
+func (el *ExecutionLogger) Debug(message string) {
+	el.Log(message, entities.BotRunLogSeverityDebug)
+}
+
+func (el *ExecutionLogger) Debugf(format string, args ...interface{}) {
+	el.Debug(fmt.Sprintf(format, args...))
+}
+
+func (el *ExecutionLogger) Info(message string) {
+	el.Log(message, entities.BotRunLogSeverityInfo)
+}
+
+func (el *ExecutionLogger) Infof(format string, args ...interface{}) {
+	el.Info(fmt.Sprintf(format, args...))
+}
+
+func (el *ExecutionLogger) Warning(message string) {
+	el.Log(message, entities.BotRunLogSeverityWarning)
+}
+
+func (el *ExecutionLogger) Warningf(format string, args ...interface{}) {
+	el.Warning(fmt.Sprintf(format, args...))
+}
+
+func (el *ExecutionLogger) Error(message string) {
+	el.Log(message, entities.BotRunLogSeverityError)
+}
+
+func (el *ExecutionLogger) Errorf(format string, args ...interface{}) {
+	el.Error(fmt.Sprintf(format, args...))
+}
+
+func (el *ExecutionLogger) Fatal(message string) {
+	el.Log(message, entities.BotRunLogSeverityFatal)
+}
+
+func (el *ExecutionLogger) Fatalf(format string, args ...interface{}) {
+	el.Fatal(fmt.Sprintf(format, args...))
+}
