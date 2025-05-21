dc22ed55a60ac5bc76043d0b97499ded693ca478
vincent-tr <vincent.tr@gmail.com>
2025-05-21 12:00:59 +0200

    remove legacy nodejs server tools

--- packages/mylife-tools-build/bin/server-build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env /bin/bash
-
-../mylife-tools-build/node_modules/.bin/webpack --config=../mylife-tools-build/server/index.ts --progress --mode=production
--- packages/mylife-tools-build/bin/server-watch.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env /bin/bash
-
-../mylife-tools-build/node_modules/.bin/webpack watch --config=../mylife-tools-build/server/index.ts --progress --mode=development
