8dd4776bf4eb9c7faabced75dc09fdecf87b5422
vincent-tr <vincent.tr@gmail.com>
2025-10-17 18:00:07 +0200

    Move folders + UI build OK

---
+++ common/mylife-tools-build/bin/ui-watch-vite.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env /bin/bash
+
+../../node_modules/.bin/vite --config ../../common/mylife-tools-build/ui/vite.config.ts
--- packages/mylife-tools-build/bin/ui-build.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env /bin/bash
-
-../../node_modules/.bin/tsc --project public/tsconfig.json && \
-../../node_modules/.bin/vite --config ../mylife-tools-build/ui/vite.config.ts build
