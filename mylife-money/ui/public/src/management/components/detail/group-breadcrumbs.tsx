eda078231a318f26e8acec18f2a03d2232419b01
vincent-tr <vincent.tr@gmail.com>
2025-11-19 15:38:02 +0100

    WIP adapt money

--- mylife-money/ui/public/src/management/components/detail/group-breadcrumbs.tsx
+++ mylife-money/ui/public/src/management/components/detail/group-breadcrumbs.tsx
@@ -2,26 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import icons from '../../../common/icons';
 import GroupSelectorButton from '../../../common/components/group-selector-button';
-import { Tooltip, Breadcrumbs, Link, makeStyles } from '@mui/material';
+import { Tooltip, Breadcrumbs, Link, styled } from '@mui/material';
 
-const useStyles = makeStyles(theme => ({
-  container: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  breadcrumbs: {
-    flex: '1 1 auto',
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-  },
+const Container = styled('div')({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center'
+});
+
+const GroupPath = styled(Breadcrumbs)(({ theme }) => ({
+  flex: '1 1 auto',
+  marginLeft: theme.spacing(1),
+  marginRight: theme.spacing(1),
 }));
 
 const GroupBreadcrumbs = ({ groupStack, onMove, onOpenGroup }) => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.container}>
+    <Container>
       <Tooltip title={'Déplacer l\'opération'}>
         <div>
           <GroupSelectorButton onSelect={onMove}>
@@ -30,7 +27,7 @@ const GroupBreadcrumbs = ({ groupStack, onMove, onOpenGroup }) => {
         </div>
       </Tooltip>
 
-      <Breadcrumbs aria-label='breadcrumb' className={classes.breadcrumbs}>
+      <GroupPath aria-label='breadcrumb'>
         {groupStack.map(group => {
           const handleClick = e => {
             e.preventDefault();
@@ -43,8 +40,8 @@ const GroupBreadcrumbs = ({ groupStack, onMove, onOpenGroup }) => {
             </Link>
           );
         })}
-      </Breadcrumbs>
-    </div>
+      </GroupPath>
+    </Container>
   );
 };
 
